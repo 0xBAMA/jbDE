757d88e6347013575edda73f7db4a0ae19844703
jb <jon.baker1222@gmail.com>
2023-03-28 19:02:13 -0500

    refactor constructor

--- src/engine/coreUtils/random.h
+++ src/engine/coreUtils/random.h
@@ -4,23 +4,21 @@
 
 #include <random>
 
+std::random_device r;
+
 // simplifying the use of std::random, so I don't have to look it up every single time
 template < typename T > class rng {
 public:
 
 	// generate a seed from the system's random device
-	rng ( T min, T max ) : minVal( min ), maxVal( max ) {
-		std::random_device r;
-		std::seed_seq seed{ r(), r(), r(), r(), r(), r(), r(), r(), r() };
-		generator = std::mt19937_64( seed );
-		distribution = std::uniform_real_distribution< T >( minVal, maxVal );
-	}
+	rng ( T min, T max ) :
+		generator( std::mt19937_64( std::seed_seq( r(),r(),r(),r(),r(),r(),r(),r(),r() ) ) ),
+		distribution( std::uniform_real_distribution< T >( min, max ) ) {}
 
-	// take 32 bit input seed value
-	rng ( T min, T max, uint32_t seed ) : minVal( min ), maxVal( max ) {
-		generator = std::mt19937_64( seed );
-		distribution = std::uniform_real_distribution< T >( minVal, maxVal );
-	}
+	// alternatively, take 32 bit known seed value
+	rng ( T min, T max, uint32_t seed ) :
+		generator( std::mt19937_64( seed ) ),
+		distribution( std::uniform_real_distribution< T >( min, max ) ) {}
 
 	T get () {
 		return T( distribution( generator ) );
@@ -29,10 +27,10 @@ public:
 private:
 	std::mt19937_64 generator;
 	std::uniform_real_distribution< T > distribution;
-	T minVal, maxVal; // not sure if these need to be kept...
 };
 
-// seedable, deterministic versions ( mt19937 is seedable+deterministic, just need to figure out how to seed )
+
+// other potential future options... maybe not neccesary, given the seedability of the mt19937_64 object
 	// wang hash generator
 	// LCG generator
 
