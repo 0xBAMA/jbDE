c973eee27f3cf4c7c52c923d85a656272acb680a
jb <jon.baker1222@gmail.com>
2023-03-31 21:43:44 -0500

    derived class coming together

--- src/engine/engine.h
+++ src/engine/engine.h
@@ -72,55 +72,51 @@ protected:
 	bool pQuit = false;
 };
 
-// placeholder - will return to this soon, once the child virtual funcs are doing something non-trivial
-class engineChild : public engine {
+
+class engineChild : public engine {	// example derived class
 public:
 	engineChild () { Init(); OnInit(); PostInit(); }
 	~engineChild () { Quit(); }
 
-	bool MainLoop () {
+	bool MainLoop () {				// this is what's called from the loop in main
 		ZoneScoped;
 
-		HandleTridentEvents();		// event handling - need to add custom event handling for the derived class
+		HandleTridentEvents();		// event handling
+		HandleCustomEvents();
 		HandleQuitEvents();
 
+		// derived-class-specific functionality
+		OnUpdate();
+		OnRender();
+
+		FrameMark;					// tells tracy that this is the end of a frame
+		PrepareProfilingData();		// get profiling data ready for next frame
+		return pQuit;
+	}
+
+	void HandleCustomEvents () {
+		// application specific controls
+	}
+
+	void OnInit () {
+		{ Block Start( "Additional User Init" );
+			// currently this is mostly for feature testing in oneShot mode
+
+			// this will also contain application specific textures, shaders, and bindsets
+		}
+	}
+
+	void OnUpdate () {
+		// application-specific update code
+	}
+
+	void OnRender () {
 		ClearColorAndDepth();		// if I just disable depth testing, this can disappear
 		DrawAPIGeometry();			// draw any API geometry desired
 		ComputePasses();			// multistage update of displayTexture
 		BlitToScreen();				// fullscreen triangle copying to the screen
 		ImguiPass();				// do all the gui stuff - this needs to be broken out into more pieces
 		window.Swap();				// show what has just been drawn to the back buffer
-		FrameMark;					// tells tracy that this is the end of a frame
-		PrepareProfilingData();		// get profiling data ready for next frame
-		return pQuit;
-	}
-
-	void OnInit () {
-		{ Block Start( "Additional User Init" ); // currently this is mostly for feature testing in oneShot mode
-			// rng gen( 0, 500, 42069 );
-			// Image_4U histogram( 500, 500 );
-			// for ( uint32_t i{ 0 }; i < 100000; i++ ) {
-			// 	int val = int( gen() );
-			// 	for ( uint32_t y{ 0 }; y < histogram.Height(); y++ ) {
-			// 		color_4U read = histogram.GetAtXY( val, y );
-			// 		if ( read[ alpha ] == 0 ) {
-			// 			read[ red ] = read[ green ] = read[ blue ] = read[ alpha ] = 255 * ( i / 100000.0f );
-			// 			histogram.SetAtXY( val, y, read );
-			// 			break;
-			// 		}
-			// 	}
-			// }
-			// histogram.FlipVertical();
-			// histogram.Save( "histogram.png" );
-		}
-	}
-
-	void OnUpdate () {
-
-	}
-
-	void OnRender () {
-		
 	}
 };
 
