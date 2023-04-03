271fc4346e5edb1030f992c73fd61ad6a9d5f0e3
jb <jon.baker1222@gmail.com>
2023-04-02 20:51:48 -0500

    reorganize build stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,10 @@ mkdir build
 cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release
 cd build
 # -j arg is max jobs + 1, here configured for 16 compilation threads
+# time make -j17 TracyClient
 time make -j17 engineDemo
 time make -j17 SoftBodies
+time make -j17 Vertexture
 cd ..
 
 if [ "$1" == "noiseTool" ]
