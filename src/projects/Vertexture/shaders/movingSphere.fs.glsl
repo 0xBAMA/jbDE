a0e4d027aa834d9dc3becb33b7e322d11ca8bc57
jb <jon.baker1222@gmail.com>
2023-05-15 00:14:28 -0500

    basic phong kinda working for moving and static spheres

--- src/projects/Vertexture/shaders/movingSphere.fs.glsl
+++ src/projects/Vertexture/shaders/movingSphere.fs.glsl
@@ -3,6 +3,8 @@
 uniform sampler2D sphere;
 uniform mat3 trident;
 uniform float time;
+uniform float scale;
+uniform float frameHeight;
 
 // moving lights state
 uniform int lightCount;
@@ -30,11 +32,24 @@ void main () {
 	vec3 normal = inverse( rot ) * vec3( 2.0f * ( gl_PointCoord.xy - vec2( 0.5f ) ), -tRead.x );
 	vec3 worldPosition = position + normal * ( radius / frameHeight );
 
-	// lighting calculations
-	vec3 lightColor = vec3( 0.0f );
+	// lighting
+	const vec3 eyePosition = vec3( 0.0f, 0.0f, -1.0f );
+	vec3 lightContribution = vec3( 0.0f );
 	for ( int i = 0; i < lightCount; i++ ) {
-		const float lightDist = distance( rot * lightData[ i ].position.xyz, position );
-		lightColor += lightData[ i ].color.rgb * ( 1.0f / lightDist );
+		const vec3 lightLocation = scale * trident * lightData[ i ].position.xyz;
+
+		// phong setup
+		const vec3 lightVector = normalize( worldPosition - lightLocation );
+		const vec3 viewVector = normalize( worldPosition - eyePosition );
+		const vec3 reflectedVector = normalize( reflect( lightVector, normal ) );
+
+		// lighting calculation
+		const float lightDot = dot( normal, lightVector );
+		const float distanceFactor = 1.0f / ( pow( distance( worldPosition, lightLocation ) / scale, 2.0f ) );
+		const float diffuseContribution = distanceFactor * max( lightDot, 0.0f );
+		const float specularContribution = distanceFactor * pow( max( dot( reflectedVector, viewVector ), 0.0f ), 60.0f );
+
+		lightContribution += lightData[ i ].color.rgb * ( diffuseContribution + ( ( lightDot > 0.0f ) ? specularContribution : 0.0f ) );
 	}
 
 	gl_FragDepth = gl_FragCoord.z + ( ( radius / frameHeight ) * ( 1.0f - tRead.x ) );
--- src/projects/Vertexture/shaders/sphere.fs.glsl
+++ src/projects/Vertexture/shaders/sphere.fs.glsl
@@ -1,6 +1,9 @@
 #version 430
 
 uniform sampler2D sphere;
+uniform float time;
+uniform float scale;
+uniform float AR;
 uniform mat3 trident;
 uniform float frameHeight;
 
@@ -19,7 +22,6 @@ layout( binding = 4, std430 ) buffer lightDataBuffer {
 in float radius;
 in vec3 color;
 in vec3 position;
-in mat3 rot;
 flat in int index; // flat means no interpolation across primitive
 
 out vec4 glFragColor;
@@ -32,11 +34,24 @@ void main () {
 	vec3 normal = inverse( trident ) * vec3( 2.0f * ( gl_PointCoord.xy - vec2( 0.5f ) ), -tRead.x );
 	vec3 worldPosition = position + normal * ( radius / frameHeight );
 
-	// lighting calculations
-	vec3 lightColor = vec3( 0.0f );
+	// lighting
+	const vec3 eyePosition = vec3( 0.0f, 0.0f, -1.0f );
+	vec3 lightContribution = vec3( 0.0f );
 	for ( int i = 0; i < lightCount; i++ ) {
-		const float lightDist = distance( rot * lightData[ i ].position.xyz, position );
-		lightColor += lightData[ i ].color.rgb * ( 1.0f / lightDist );
+		const vec3 lightLocation = scale * trident * lightData[ i ].position.xyz;
+
+		// phong setup
+		const vec3 lightVector = normalize( worldPosition - lightLocation );
+		const vec3 viewVector = normalize( worldPosition - eyePosition );
+		const vec3 reflectedVector = normalize( reflect( lightVector, normal ) );
+
+		// lighting calculation
+		const float lightDot = dot( normal, lightVector );
+		const float distanceFactor = 1.0f / ( pow( distance( worldPosition, lightLocation ) / scale, 2.0f ) );
+		const float diffuseContribution = distanceFactor * max( lightDot, 0.0f );
+		const float specularContribution = distanceFactor * pow( max( dot( reflectedVector, viewVector ), 0.0f ), 60.0f );
+
+		lightContribution += lightData[ i ].color.rgb * ( diffuseContribution + ( ( lightDot > 0.0f ) ? specularContribution : 0.0f ) );
 	}
 
 	gl_FragDepth = gl_FragCoord.z + ( ( radius / frameHeight ) * ( 1.0f - tRead.x ) );
