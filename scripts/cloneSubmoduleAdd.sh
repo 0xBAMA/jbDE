034a1c14a4ec0c19ed5844af9a28db1025f60850
jb <43890665+0xBAMA@users.noreply.github.com>
2023-05-22 16:21:57 -0500

    fix paths

--- scripts/cloneSubmoduleAdd.sh
+++ scripts/cloneSubmoduleAdd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git submodule add https://github.com/Auburn/FastNoise2.git src/noise/FastNoise2
-git submodule add https://github.com/wolfpld/tracy.git src/tracy
-git submodule add https://github.com/Raikiri/LegitProfiler.git src/ImGUI/LegitProfiler
+git submodule add https://github.com/Auburn/FastNoise2.git src/utils/noise/FastNoise2
+git submodule add https://github.com/wolfpld/tracy.git src/utils/tracy
+git submodule add https://github.com/Raikiri/LegitProfiler.git src/utils/ImGUI/LegitProfiler
