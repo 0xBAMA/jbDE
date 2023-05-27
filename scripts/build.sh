e5d668c96f9e84afecf82c66bf99b45f665860df
0xBAMA <jon.baker1222@gmail.com>
2023-05-27 09:56:37 -0500

    add AirplaneMode2 child applicaiton

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,13 @@ mkdir build
 cmake -S . -B ./build -DCMAKE_BUILD_TYPE=Release
 cd build
 # -j arg is max jobs + 1, here configured for 16 compilation threads
+
+# engine stuff
 # time make -j17 TracyClient
-time make -j17 engineDemo
+# time make -j17 engineDemo
+
+# child apps
+time make -j17 AirplaneMode2
 # time make -j17 FontGame
 # time make -j17 Physarum
 # time make -j17 Siren
@@ -13,7 +18,9 @@ time make -j17 engineDemo
 # time make -j17 Vertexture
 # time make -j17 Voraldo14
 # time make -j17 VoxelSpace
-# time make -j5 # so apparently the default target builds everything in a parallel fashion - need to test to see if this is faster
+
+# so apparently the default target builds everything in a parallel fashion - need to test to see if this is faster
+# time make -j5 # I think it would also build the noise tool, tbd
 cd ..
 
 if [ "$1" == "noiseTool" ]
