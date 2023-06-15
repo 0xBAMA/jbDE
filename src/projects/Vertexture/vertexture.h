32b9f7230a22a87204c10473b55598cbbd7da56a
jb <jon.baker1222@gmail.com>
2023-06-15 04:07:36 -0500

    render target creation bugfix

--- src/projects/Vertexture/vertexture.h
+++ src/projects/Vertexture/vertexture.h
@@ -323,7 +323,7 @@ void APIGeometryContainer::Initialize () {
 	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 
 	glActiveTexture( GL_TEXTURE21 );
-	glBindTexture( GL_TEXTURE_2D, fbPosition[ 0 ] );
+	glBindTexture( GL_TEXTURE_2D, fbPosition[ 1 ] );
 	glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA16F, config.width, config.height, 0, GL_RGBA, GL_FLOAT, NULL );
 	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
 	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
