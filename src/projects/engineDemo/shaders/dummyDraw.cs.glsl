11ee433fdd492589308b1b541ea148e80df89e69
0xBAMA <jon.baker1222@gmail.com>
2023-06-23 18:41:37 -0500

    fix dummy draw shader

--- src/projects/engineDemo/shaders/dummyDraw.cs.glsl
+++ src/projects/engineDemo/shaders/dummyDraw.cs.glsl
@@ -1,7 +1,8 @@
 #version 430
 layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
 layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
-layout( binding = 1, rgba8ui ) uniform uimage2D accumulatorTexture;
+// layout( binding = 1, rgba8ui ) uniform uimage2D accumulatorTexture;
+layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
 
 void main () {
 	// basic XOR pattern
@@ -14,5 +15,5 @@ void main () {
 	result += imageLoad( blueNoiseTexture, writeLoc % imageSize( blueNoiseTexture ) ).xyz;
 
 	// write the data to the image
-	imageStore( accumulatorTexture, writeLoc, uvec4( result, 255 ) );
+	imageStore( accumulatorTexture, writeLoc, vec4( result / 255.0f, 1.0f ) );
 }
