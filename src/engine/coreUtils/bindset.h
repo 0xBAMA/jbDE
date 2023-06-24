08f3af1a3c2884273edcdc2b10d0efff775cb702
jb <jon.baker1222@gmail.com>
2023-06-24 14:15:06 -0500

    bindset notes

--- src/engine/coreUtils/bindset.h
+++ src/engine/coreUtils/bindset.h
@@ -2,6 +2,15 @@
 need to handle the binding of images to the binding points for each shader
 	- this will grow to include both image and texture usage
 		*** texture manager will need to change to make use of this stuff, too
+
+		- in the case where I need to get the shader program handle, I can do a
+		glGet with GL_CURRENT_PROGRAM, which will give me the last thing that
+		got set up with glUseProgram - this is an easy way to keep the interface
+		the same, keeps from having to do anything to different to pass in the
+		information
+			- bindsets can be then program-agnostic, kind of, they might need
+			to know that it's the same name shared both sides of the bus, or
+			something like that
 ==============================================================================*/
 struct binding {
 	binding( int bp, GLuint te, GLenum ty ) : bindPoint( bp ), texture( te ), type( ty ) {}
