291c72391e8130bd97c99837f77232cce0fef824
jb <jon.baker1222@gmail.com>
2023-06-27 20:35:21 -0500

    shitty attempt at getting cpu string ( not working )

--- src/engine/engineInit.cc
+++ src/engine/engineInit.cc
@@ -82,6 +82,30 @@ void engineBase::DisplaySetup () {
 		// CPU information
 		cout << T_BLUE << "    CPU Info :" << RESET << newline;
 
+		{
+			// // from https://stackoverflow.com/questions/850774/how-to-determine-the-hardware-cpu-and-ram-on-a-machine
+			// #include <cpuid.h>
+
+			// char CPUBrandString[ 0x40 ];
+			// unsigned int CPUInfo[ 4 ] = { 0, 0, 0, 0 };
+
+			// __cpuid( 0x80000000, CPUInfo[ 0 ], CPUInfo[ 1 ], CPUInfo[ 2 ], CPUInfo[ 3 ] );
+
+			// unsigned int nExIds = CPUInfo[ 0 ];
+			// memset( CPUBrandString, 0, sizeof( CPUBrandString ) );
+			// for ( unsigned int i = 0x80000000; i <= nExIds; ++i ) {
+			// 	__cpuid( i, CPUInfo[ 0 ], CPUInfo[ 1 ], CPUInfo[ 2 ], CPUInfo[ 3 ] );
+
+			// 	if ( i == 0x80000002 )
+			// 		memcpy( CPUBrandString, CPUInfo, sizeof( CPUInfo ) );
+			// 	else if ( i == 0x80000003 )
+			// 		memcpy( CPUBrandString + 16, CPUInfo, sizeof( CPUInfo ) );
+			// 	else if ( i == 0x80000004 )
+			// 		memcpy( CPUBrandString + 32, CPUInfo, sizeof( CPUInfo ) );
+			// }
+			// cout << "CPU Type: " << CPUBrandString << endl;
+		}
+
 		cout << T_RED << "      Logical Cores : " << T_CYAN << SDL_GetCPUCount() << newline;
 		cout << T_RED << "      System RAM : " << T_CYAN << SDL_GetSystemRAM() << " MB" << newline;
 		// cout << T_RED << "      Vendor : " << T_CYAN << vendor << RESET << newline;
