6fe7423fef4521f010802767a91e3a813e47f0b1
jb <jon.baker1222@gmail.com>
2023-07-04 22:13:00 -0500

    physarum sim working - couple pieces pending refactor

--- src/projects/Physarum/main.cc
+++ src/projects/Physarum/main.cc
@@ -5,6 +5,9 @@ struct physarumConfig_t {
 	uint32_t numAgents;
 	uint32_t dimensionX;
 	uint32_t dimensionY;
+	bool showTrails;
+	bool showAgents;
+	float agentPointsize;
 
 	// agent sim
 	float senseAngle;
@@ -25,6 +28,8 @@ public:
 	physarumConfig_t physarumConfig;
 
 	GLuint agentSSBO;
+	GLuint continuumVAO;
+	GLuint continuumVBO;
 
 	void OnInit () {
 		ZoneScoped;
@@ -43,6 +48,9 @@ public:
 			physarumConfig.numAgents		= j[ "app" ][ "Physarum" ][ "numAgents" ];
 			physarumConfig.dimensionX		= j[ "app" ][ "Physarum" ][ "dimensionX" ];
 			physarumConfig.dimensionY		= j[ "app" ][ "Physarum" ][ "dimensionY" ];
+			physarumConfig.showTrails		= j[ "app" ][ "Physarum" ][ "showTrails" ];
+			physarumConfig.showAgents		= j[ "app" ][ "Physarum" ][ "showAgents" ];
+			physarumConfig.agentPointsize	= j[ "app" ][ "Physarum" ][ "agentPointsize" ];
 			physarumConfig.senseAngle		= j[ "app" ][ "Physarum" ][ "senseAngle" ];
 			physarumConfig.senseDistance	= j[ "app" ][ "Physarum" ][ "senseDistance" ];
 			physarumConfig.turnAngle		= j[ "app" ][ "Physarum" ][ "turnAngle" ];
@@ -61,13 +69,58 @@ public:
 			rng dist( -1.0f, 1.0f );
 
 			for ( uint32_t i = 0; i < physarumConfig.numAgents; i++ ) {
-				agentsInitialData.push_back( { { dist(), dist() }, glm::normalize( { dist(), dist() } ) } );
+				agentsInitialData.push_back( { { dist(), dist() }, glm::normalize( vec2( dist(), dist() ) ) } );
 			}
 
 			glGenBuffers( 1, &agentSSBO );
 			glBindBuffer( GL_SHADER_STORAGE_BUFFER, agentSSBO );
 			glBufferData( GL_SHADER_STORAGE_BUFFER, bufferSize, ( GLvoid * ) &agentsInitialData[ 0 ], GL_DYNAMIC_COPY );
 			glBindBufferBase( GL_SHADER_STORAGE_BUFFER, 0, agentSSBO );
+
+
+		// pending refactor
+
+			// set up the points for the continuum
+			//  A---------------B
+			//  |          .    |
+			//  |       .       |
+			//  |    .          |
+			//  |               |
+			//  C---------------D
+
+			// diagonal runs from C to B
+			//  A is -1, 1
+			//  B is  1, 1
+			//  C is -1,-1
+			//  D is  1,-1
+			std::vector< glm::vec3 > points;
+			points.push_back( glm::vec3( -1.0f,  1.0f, 0.5f ) ); //A
+			points.push_back( glm::vec3( -1.0f, -1.0f, 0.5f ) ); //C
+			points.push_back( glm::vec3(  1.0f,  1.0f, 0.5f ) ); //B
+			points.push_back( glm::vec3(  1.0f,  1.0f, 0.5f ) ); //B
+			points.push_back( glm::vec3( -1.0f, -1.0f, 0.5f ) ); //C
+			points.push_back( glm::vec3(  1.0f, -1.0f, 0.5f ) ); //D
+
+			// vao, vbo
+			cout << endl << "  setting up vao, vbo for continuum model...";
+			glGenVertexArrays( 1, &continuumVAO );
+			glBindVertexArray( continuumVAO );
+			glGenBuffers( 1, &continuumVBO );
+			glBindBuffer( GL_ARRAY_BUFFER, continuumVBO );
+			cout << "done." << endl;
+
+			// buffer the data
+			cout << "  buffering vertex data...";
+			glBufferData( GL_ARRAY_BUFFER, sizeof( glm::vec3 ) * points.size(), NULL, GL_DYNAMIC_DRAW );
+			glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof( glm::vec3 ) * points.size(), &points[ 0 ] );
+			cout << "done." << endl;
+
+			// set up attributes
+			cout << "  setting up attributes in continuum shader...";
+			GLuint pointsAttrib = glGetAttribLocation( shaders[ "Continuum" ], "vPosition" );
+			glEnableVertexAttribArray( pointsAttrib );
+			glVertexAttribPointer( pointsAttrib, 3, GL_FLOAT, GL_FALSE, 0, ( GLvoid* ) 0 );
+			cout << "done." << endl;
 
 			// setup the image buffers for the atomic writes ( 2x for ping-ponging )
 			textureOptions_t opts;
@@ -102,8 +155,38 @@ public:
 
 	void DrawAPIGeometry () {
 		ZoneScoped; scopedTimer Start( "API Geometry" );
-		// draw some shit
 
+		glUseProgram( shaders[ "Continuum" ] );
+		glBindVertexArray( continuumVAO );
+		glBindBuffer( GL_ARRAY_BUFFER, continuumVBO );
+		glUniform1i( glGetUniformLocation( shaders[ "Continuum" ], "show_trails" ), physarumConfig.showTrails );
+		glDrawArrays( GL_TRIANGLES, 0, 6 );
+
+		// agents
+		glUseProgram( shaders[ "Agents" ] );
+		glPointSize( physarumConfig.agentPointsize );
+
+		// generation of the random values to be used in the shader
+		std::vector< glm::vec2 > randomDirections;
+		long unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+
+		std::default_random_engine gen{ seed };
+		std::uniform_real_distribution< GLfloat > dist{ -1.0f, 1.0f };
+
+		for ( int i = 0; i < 8; i++ )
+			randomDirections.push_back( glm::normalize( vec2( dist( gen ), dist( gen ) ) ) );
+
+		glUniform2fv( glGetUniformLocation( shaders[ "Agents" ], "random_values" ), 8, glm::value_ptr( randomDirections[ 0 ] ) );
+
+		// the rest of the simulation parameters
+		glUniform1i( glGetUniformLocation( shaders[ "Agents" ], "show_agents" ), physarumConfig.showAgents );
+		glUniform1f( glGetUniformLocation( shaders[ "Agents" ], "step_size" ), physarumConfig.stepSize );
+		glUniform1f( glGetUniformLocation( shaders[ "Agents" ], "sense_angle" ), physarumConfig.senseAngle );
+		glUniform1f( glGetUniformLocation( shaders[ "Agents" ], "sense_distance" ), physarumConfig.senseDistance );
+		glUniform1f( glGetUniformLocation( shaders[ "Agents" ], "turn_angle" ), physarumConfig.turnAngle );
+		glUniform1ui( glGetUniformLocation( shaders[ "Agents" ], "deposit_amount" ), physarumConfig.depositAmount );
+
+		glDrawArrays( GL_POINTS, 0, physarumConfig.numAgents / 2 );
 	}
 
 	void ComputePasses () {
@@ -143,17 +226,18 @@ public:
 	void OnUpdate () {
 		ZoneScoped; scopedTimer Start( "Update" );
 
-		// // run the shader to do the gaussian blur
-		// glUseProgram( diffuse_and_decay_shader );
+		// run the shader to do the gaussian blur
+		glUseProgram( shaders[ "Diffuse and Decay" ] );
 
-		// //swap the images
-		// std::swap( continuum_textures[ 0 ], continuum_textures[ 1 ] );
-		// glBindImageTexture( 1, continuum_textures[ 0 ], 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
-		// glBindImageTexture( 2, continuum_textures[ 1 ], 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
-		// glUniform1f( glGetUniformLocation( diffuse_and_decay_shader, "decay_factor" ), decay_factor );
+		//swap the images
+		static bool oddFrame = false;
+		glBindImageTexture( 1, textureManager.Get( string( "Pheremone Continuum Buffer " ) + string( oddFrame ? "0" : "1" ) ), 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
+		glBindImageTexture( 2, textureManager.Get( string( "Pheremone Continuum Buffer " ) + string( oddFrame ? "1" : "0" ) ), 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
+		glUniform1f( glGetUniformLocation( shaders[ "Diffuse and Decay" ], "decay_factor" ), physarumConfig.decayFactor );
+		oddFrame = !oddFrame;
 
-		// glDispatchCompute( DIM / 8, DIM / 8, 1 );
-		// glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		glDispatchCompute( ( physarumConfig.dimensionX + 7 ) / 8, ( physarumConfig.dimensionY + 7 ) / 8, 1 );
+		glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
 
 	}
 
