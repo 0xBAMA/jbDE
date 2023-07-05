3a44d8745533a60f8b112d9ade7b1bb21a0f2f37
jb <jon.baker1222@gmail.com>
2023-07-04 23:00:21 -0500

    refactor vert/frag -> compute display of continuum

--- src/projects/Physarum/main.cc
+++ src/projects/Physarum/main.cc
@@ -8,6 +8,7 @@ struct physarumConfig_t {
 	bool showTrails;
 	bool showAgents;
 	float agentPointsize;
+	bool oddFrame = false;
 
 	// agent sim
 	float senseAngle;
@@ -38,10 +39,9 @@ public:
 
 			// something to put some basic data in the accumulator texture - comes from the demo project
 			const string basePath = "./src/projects/Physarum/shaders/";
-			shaders[ "Dummy Draw" ]			= computeShader( basePath + "dummyDraw.cs.glsl" ).shaderHandle;
+			shaders[ "Buffer Copy" ]		= computeShader( basePath + "bufferCopy.cs.glsl" ).shaderHandle;
 			shaders[ "Diffuse and Decay" ]	= computeShader( basePath + "diffuseAndDecay.cs.glsl" ).shaderHandle;
 			shaders[ "Agents" ]				= regularShader( basePath + "agent.vs.glsl", basePath + "agent.fs.glsl" ).shaderHandle;
-			shaders[ "Continuum" ]			= regularShader( basePath + "continuum.vs.glsl", basePath + "continuum.fs.glsl" ).shaderHandle;
 
 			// get the configuration from config.json
 			json j; ifstream i ( "src/engine/config.json" ); i >> j; i.close();
@@ -77,57 +77,14 @@ public:
 			glBufferData( GL_SHADER_STORAGE_BUFFER, bufferSize, ( GLvoid * ) &agentsInitialData[ 0 ], GL_DYNAMIC_COPY );
 			glBindBufferBase( GL_SHADER_STORAGE_BUFFER, 0, agentSSBO );
 
-
-		// pending refactor
-
-			// set up the points for the continuum
-			//  A---------------B
-			//  |          .    |
-			//  |       .       |
-			//  |    .          |
-			//  |               |
-			//  C---------------D
-
-			// diagonal runs from C to B
-			//  A is -1, 1
-			//  B is  1, 1
-			//  C is -1,-1
-			//  D is  1,-1
-			std::vector< glm::vec3 > points;
-			points.push_back( glm::vec3( -1.0f,  1.0f, 0.5f ) ); //A
-			points.push_back( glm::vec3( -1.0f, -1.0f, 0.5f ) ); //C
-			points.push_back( glm::vec3(  1.0f,  1.0f, 0.5f ) ); //B
-			points.push_back( glm::vec3(  1.0f,  1.0f, 0.5f ) ); //B
-			points.push_back( glm::vec3( -1.0f, -1.0f, 0.5f ) ); //C
-			points.push_back( glm::vec3(  1.0f, -1.0f, 0.5f ) ); //D
-
-			// vao, vbo
-			cout << endl << "  setting up vao, vbo for continuum model...";
-			glGenVertexArrays( 1, &continuumVAO );
-			glBindVertexArray( continuumVAO );
-			glGenBuffers( 1, &continuumVBO );
-			glBindBuffer( GL_ARRAY_BUFFER, continuumVBO );
-			cout << "done." << endl;
-
-			// buffer the data
-			cout << "  buffering vertex data...";
-			glBufferData( GL_ARRAY_BUFFER, sizeof( glm::vec3 ) * points.size(), NULL, GL_DYNAMIC_DRAW );
-			glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof( glm::vec3 ) * points.size(), &points[ 0 ] );
-			cout << "done." << endl;
-
-			// set up attributes
-			cout << "  setting up attributes in continuum shader...";
-			GLuint pointsAttrib = glGetAttribLocation( shaders[ "Continuum" ], "vPosition" );
-			glEnableVertexAttribArray( pointsAttrib );
-			glVertexAttribPointer( pointsAttrib, 3, GL_FLOAT, GL_FALSE, 0, ( GLvoid* ) 0 );
-			cout << "done." << endl;
-
 			// setup the image buffers for the atomic writes ( 2x for ping-ponging )
 			textureOptions_t opts;
 			opts.dataType		= GL_R32UI;
 			opts.width			= physarumConfig.dimensionX;
 			opts.height			= physarumConfig.dimensionY;
 			opts.textureType	= GL_TEXTURE_2D;
+			// opts.magFilter		= GL_LINEAR;
+			// opts.minFilter		= GL_LINEAR;
 			textureManager.Add( "Pheremone Continuum Buffer 0", opts );
 			textureManager.Add( "Pheremone Continuum Buffer 1", opts );
 		}
@@ -155,12 +112,6 @@ public:
 
 	void DrawAPIGeometry () {
 		ZoneScoped; scopedTimer Start( "API Geometry" );
-
-		glUseProgram( shaders[ "Continuum" ] );
-		glBindVertexArray( continuumVAO );
-		glBindBuffer( GL_ARRAY_BUFFER, continuumVBO );
-		glUniform1i( glGetUniformLocation( shaders[ "Continuum" ], "show_trails" ), physarumConfig.showTrails );
-		glDrawArrays( GL_TRIANGLES, 0, 6 );
 
 		// agents
 		glUseProgram( shaders[ "Agents" ] );
@@ -195,7 +146,15 @@ public:
 		{ // dummy draw - draw something into accumulatorTexture
 			scopedTimer Start( "Drawing" );
 			bindSets[ "Drawing" ].apply();
-			glUseProgram( shaders[ "Dummy Draw" ] );
+
+			// bind the texture
+			// sample it into the accumulator
+
+			glUseProgram( shaders[ "Buffer Copy" ] );
+			glUniform1i( glGetUniformLocation( shaders[ "Buffer Copy" ], "show_trails" ), physarumConfig.showTrails );
+			glUniform2f( glGetUniformLocation( shaders[ "Buffer Copy" ], "resolution" ), config.width, config.height );
+			textureManager.BindTexForShader( string( "Pheremone Continuum Buffer " ) + string( physarumConfig.oddFrame ? "0" : "1" ), "continuum", shaders[ "Buffer Copy" ], 2 );
+
 			glDispatchCompute( ( config.width + 15 ) / 16, ( config.height + 15 ) / 16, 1 );
 			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
 		}
@@ -225,11 +184,10 @@ public:
 		glUseProgram( shaders[ "Diffuse and Decay" ] );
 
 		//swap the images
-		static bool oddFrame = false;
-		glBindImageTexture( 1, textureManager.Get( string( "Pheremone Continuum Buffer " ) + string( oddFrame ? "0" : "1" ) ), 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
-		glBindImageTexture( 2, textureManager.Get( string( "Pheremone Continuum Buffer " ) + string( oddFrame ? "1" : "0" ) ), 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
+		glBindImageTexture( 1, textureManager.Get( string( "Pheremone Continuum Buffer " ) + string( physarumConfig.oddFrame ? "0" : "1" ) ), 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
+		glBindImageTexture( 2, textureManager.Get( string( "Pheremone Continuum Buffer " ) + string( physarumConfig.oddFrame ? "1" : "0" ) ), 0, GL_FALSE, 0, GL_READ_WRITE, GL_R32UI );
 		glUniform1f( glGetUniformLocation( shaders[ "Diffuse and Decay" ], "decay_factor" ), physarumConfig.decayFactor );
-		oddFrame = !oddFrame;
+		physarumConfig.oddFrame = !physarumConfig.oddFrame;
 
 		glDispatchCompute( ( physarumConfig.dimensionX + 7 ) / 8, ( physarumConfig.dimensionY + 7 ) / 8, 1 );
 		glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
---
+++ src/projects/Physarum/shaders/bufferCopy.cs.glsl
@@ -0,0 +1,15 @@
+#version 430
+layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
+layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
+layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
+
+uniform usampler2D continuum;
+uniform vec2 resolution;
+
+void main () {
+	const vec2 sampleLocation = ( vec2( gl_GlobalInvocationID.xy ) + vec2( 0.5f ) ) / resolution.xy;
+	uint result = texture( continuum, sampleLocation ).r;
+
+	// write the data to the accumulator
+	imageStore( accumulatorTexture, ivec2( gl_GlobalInvocationID.xy ), vec4( result / 1000000.0f, 0.0f, 0.0f, 1.0f ) );
+}
