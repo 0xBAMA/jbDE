80bf8121f2bc0e6dd8d3401a294701c95743fcd1
jb <jon.baker1222@gmail.com>
2023-07-09 04:33:16 -0500

    refactor shader logic

--- src/projects/CellularAutomata/shaders/update.cs.glsl
+++ src/projects/CellularAutomata/shaders/update.cs.glsl
@@ -8,30 +8,29 @@ layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
 layout( binding = 2, r32ui ) uniform uimage2D backBuffer;
 layout( binding = 3, r32ui ) uniform uimage2D frontBuffer;
 
+bool getStateForBit ( ivec2 location, uint bit ) {
+// read state + neighborhood from back buffer
+	uint previousState = imageLoad( backBuffer, location ).r;
+
+	uint count = 0;
+	count += imageLoad( backBuffer, location + ivec2( -1, -1 ) ).r;
+	count += imageLoad( backBuffer, location + ivec2(  0, -1 ) ).r;
+	count += imageLoad( backBuffer, location + ivec2(  1, -1 ) ).r;
+	count += imageLoad( backBuffer, location + ivec2( -1,  0 ) ).r;
+	// skip center pixel, already exists in previousState
+	count += imageLoad( backBuffer, location + ivec2(  1,  0 ) ).r;
+	count += imageLoad( backBuffer, location + ivec2( -1,  1 ) ).r;
+	count += imageLoad( backBuffer, location + ivec2(  0,  1 ) ).r;
+	count += imageLoad( backBuffer, location + ivec2(  1,  1 ) ).r;
+
+	// determine new state - Conway's Game of Life rules
+	return ( ( count == 2 || count == 3 ) && previousState == 1 ) || ( count == 3 && previousState == 0 );
+}
+
 void main () {
 	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
 
-// read state + neighborhood from back buffer
-	uint previousState = imageLoad( backBuffer, writeLoc ).r;
-
-	uint count = 0;
-	count += imageLoad( backBuffer, writeLoc + ivec2( -1, -1 ) ).r;
-	count += imageLoad( backBuffer, writeLoc + ivec2(  0, -1 ) ).r;
-	count += imageLoad( backBuffer, writeLoc + ivec2(  1, -1 ) ).r;
-	count += imageLoad( backBuffer, writeLoc + ivec2( -1,  0 ) ).r;
-	// skip center pixel, already exists in previousState
-	count += imageLoad( backBuffer, writeLoc + ivec2(  1,  0 ) ).r;
-	count += imageLoad( backBuffer, writeLoc + ivec2( -1,  1 ) ).r;
-	count += imageLoad( backBuffer, writeLoc + ivec2(  0,  1 ) ).r;
-	count += imageLoad( backBuffer, writeLoc + ivec2(  1,  1 ) ).r;
-
-	// determine new state
-	uint newState = 0;
-	if ( ( count == 2 || count == 3 ) && previousState == 1 ) {
-		newState = 1;
-	} else if ( count == 3 && previousState == 0 ) {
-		newState = 1;
-	}
+	uint newState = getStateForBit( writeLoc, 0 ) ? 1 : 0;
 
 	// write the data to the front buffer
 	imageStore( frontBuffer, writeLoc, uvec4( newState, 1, 1, 1 ) );
