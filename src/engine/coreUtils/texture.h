d69c16e0b09a61f7a8f256a4d81db2d6d7e9983a
0xBAMA <jon.baker1222@gmail.com>
2023-07-14 18:26:10 -0500

    add support for 16 bit depth component

--- src/engine/coreUtils/texture.h
+++ src/engine/coreUtils/texture.h
@@ -9,6 +9,7 @@ inline size_t bytesPerPixel ( GLint type ) {
 	// over time we'll accumulate all the ones that I use, this currently is sufficient
 	switch ( type ) {
 	// one channel formats
+	case GL_DEPTH_COMPONENT16:	return 1 * 2; break;
 	case GL_R32UI:
 	case GL_DEPTH_COMPONENT32:	return 1 * 4; break;
 
