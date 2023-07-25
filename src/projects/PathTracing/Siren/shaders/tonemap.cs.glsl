5777b9a8ca6e7187e81ea9b65409150248ab6ccd
jb <jon.baker1222@gmail.com>
2023-07-25 17:00:06 -0500

    copy of tonemap shader, with altered image types

---
+++ src/projects/PathTracing/Siren/shaders/tonemap.cs.glsl
@@ -0,0 +1,25 @@
+#version 430
+layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
+layout( binding = 0, rgba32f ) uniform image2D source;
+layout( binding = 1, rgba8ui ) uniform uimage2D displayTexture;
+
+#include "tonemap.glsl" // tonemapping curves
+
+uniform int tonemapMode;
+uniform float gamma;
+uniform vec3 colorTempAdjust;
+
+void main () {
+	ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
+
+	// temporary hack for inverted image
+	vec4 originalValue = imageLoad( source, ivec2( loc.x, imageSize( source ).y - loc.y - 1 ) );
+
+	vec3 color = tonemap( tonemapMode, colorTempAdjust * vec3( originalValue.rgb ) );
+	color = gammaCorrect( gamma, color );
+	uvec4 tonemappedValue = uvec4( uvec3( color * 255.0f ), originalValue.a * 255.0f );
+
+	// dithering?
+
+	imageStore( displayTexture, loc, tonemappedValue );
+}
