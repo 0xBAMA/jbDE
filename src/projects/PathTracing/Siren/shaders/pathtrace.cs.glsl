3e99838d4c2079347a48f7bedf74630690d27c28
jb <jon.baker1222@gmail.com>
2023-07-29 10:45:54 -0500

    rng utilities, rotate3d

--- src/projects/PathTracing/Siren/shaders/pathtrace.cs.glsl
+++ src/projects/PathTracing/Siren/shaders/pathtrace.cs.glsl
@@ -691,6 +691,51 @@ uniform float raymarchMaxDistance;
 uniform float raymarchEpsilon;
 uniform float raymarchUnderstep;
 
+mat3 rotate3D ( float angle, vec3 axis ) {
+	vec3 a = normalize( axis );
+	float s = sin( angle );
+	float c = cos( angle );
+	float r = 1.0f - c;
+	return mat3(
+		a.x * a.x * r + c,
+		a.y * a.x * r + a.z * s,
+		a.z * a.x * r - a.y * s,
+		a.x * a.y * r - a.z * s,
+		a.y * a.y * r + c,
+		a.z * a.y * r + a.x * s,
+		a.x * a.z * r + a.y * s,
+		a.y * a.z * r - a.x * s,
+		a.z * a.z * r + c
+	);
+}
+
+// random utilites
+uint seed = 0;
+uint wangHash () {
+	seed = uint( seed ^ uint( 61 ) ) ^ uint( seed >> uint( 16 ) );
+	seed *= uint( 9 );
+	seed = seed ^ ( seed >> 4 );
+	seed *= uint( 0x27d4eb2d );
+	seed = seed ^ ( seed >> 15 );
+	return seed;
+}
+
+float normalizedRandomFloat () {
+	return float( wangHash() ) / 4294967296.0f;
+}
+
+vec3 randomUnitVector () {
+	float z = normalizedRandomFloat() * 2.0f - 1.0f;
+	float a = normalizedRandomFloat() * 2.0f * PI;
+	float r = sqrt( 1.0f - z * z );
+	float x = r * cos( a );
+	float y = r * sin( a );
+	return vec3( x, y, z );
+}
+
+vec2 randomInUnitDisk () {
+	return randomUnitVector().xy;
+}
 
 uvec4 blueNoiseReference ( ivec2 location ) {
 	location += noiseOffset;
@@ -701,6 +746,10 @@ uvec4 blueNoiseReference ( ivec2 location ) {
 
 void main () {
 	uvec2 location = gl_GlobalInvocationID.xy + tileOffset.xy;
+
+	// wang hash seeded uniquely for every pixel
+	seed = wangSeed + 42069 * location.x + 451 * location.y;
+
 	uint result = blueNoiseReference( ivec2( location ) ).r;
 	imageStore( accumulatorColor, ivec2( location ), vec4( vec3( result / 255.0f ), 1.0f ) );
 }
