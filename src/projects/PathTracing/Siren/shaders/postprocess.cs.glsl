15d56435bc8d81e620beac4603183f83e6165616
jb <jon.baker1222@gmail.com>
2023-08-11 11:17:42 -0500

    add graphicaobscura mat3 saturation control to postprocess

--- src/projects/PathTracing/Siren/shaders/postprocess.cs.glsl
+++ src/projects/PathTracing/Siren/shaders/postprocess.cs.glsl
@@ -9,17 +9,33 @@ layout( rgba8 ) uniform image2D displayTexture;
 
 uniform int tonemapMode;
 uniform float gamma;
-uniform float contrast;
+uniform float saturation;
 uniform vec3 colorTempAdjust;
 uniform vec2 resolution;
+
+vec3 ApplySaturation ( in vec3 colorIn ) {
+	// https://www.graficaobscura.com/matrix/index.html
+	const float s = saturation;
+	const float oms = 1.0f - s;
+
+	// vec3 weights = vec3( 0.2990f, 0.5870f, 0.1140f ); // NTSC weights
+	vec3 weights = vec3( 0.3086f, 0.6094f, 0.0820f ); // "improved" luminance vector
+
+	return mat3(
+		oms * weights.r + s,	oms * weights.r,		oms * weights.r,
+		oms * weights.g,		oms * weights.g + s,	oms * weights.g,
+		oms * weights.b,		oms * weights.b,		oms * weights.b + s
+	) * colorIn;
+}
 
 void main () {
 	ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
 	vec2 sampleLoc = ( vec2( loc ) + vec2( 0.5f ) ) / resolution;
 	sampleLoc.y = 1.0f - sampleLoc.y;
 	vec4 originalValue = texture( sourceC, sampleLoc );
-	
-	vec3 color = tonemap( tonemapMode, colorTempAdjust * vec3( originalValue.rgb ) );
+
+	originalValue.rgb = ApplySaturation( originalValue.rgb );
+	vec3 color = tonemap( tonemapMode, colorTempAdjust * originalValue.rgb );
 	color = gammaCorrect( gamma, color );
 
 	// keeping sample count in the alpha channel
