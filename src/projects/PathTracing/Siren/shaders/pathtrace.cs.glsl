ce8e475701cda639720e01bea9524e3db214d1e8
jb <jon.baker1222@gmail.com>
2023-08-13 22:46:46 -0500

    add color for temperature function

--- src/projects/PathTracing/Siren/shaders/pathtrace.cs.glsl
+++ src/projects/PathTracing/Siren/shaders/pathtrace.cs.glsl
@@ -76,6 +76,17 @@ vec2 RandomInUnitDisk () {
 	return RandomUnitVector().xy;
 }
 
+vec3 GetColorForTemperature ( float temperature ) {
+	mat3 m = ( temperature <= 6500.0f )
+		? mat3( vec3( 0.0f, -2902.1955373783176f, -8257.7997278925690f ),
+				vec3( 0.0f, 1669.5803561666639f, 2575.2827530017594f ),
+				vec3( 1.0f, 1.3302673723350029f, 1.8993753891711275f ) )
+		: mat3( vec3( 1745.0425298314172f, 1216.6168361476490f, -8257.7997278925690f ),
+				vec3( -2666.3474220535695f, -2173.1012343082230f, 2575.2827530017594f ),
+				vec3( 0.55995389139931482f, 0.70381203140554553f, 1.8993753891711275f ) );
+	return mix( clamp( vec3( m[ 0 ] / ( vec3( clamp( temperature, 1000.0f, 40000.0f ) ) + m[ 1 ] )
+		+ m[ 2 ] ), vec3( 0.0f ), vec3( 1.0f ) ), vec3( 1.0f ), smoothstep( 1000.0f, 0.0f, temperature ) );
+}
 
 float Reflectance ( float cosTheta, float IoR ) {
 	// Use Schlick's approximation for reflectance
