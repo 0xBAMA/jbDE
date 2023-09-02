e98cb09a945bf234ace9b77dc6c577e5f172536d
jb <jon.baker1222@gmail.com>
2023-09-01 20:20:10 -0500

    plane intersection function

--- src/projects/PathTracing/Siren/shaders/pathtrace.cs.glsl
+++ src/projects/PathTracing/Siren/shaders/pathtrace.cs.glsl
@@ -767,6 +767,16 @@ Intersection IntersectSphere ( in vec3 origin, in vec3 direction, in vec3 center
 // 		vec4( tF, normalize( -sign( direction ) * step( t2, vec3( tF ) ) ) ) );
 // }
 
+// just solve for t, < ro+t*d, nor > - k = 0
+Intersection iPlane ( in vec3 ro, in vec3 rd, vec4 pla ) {
+    float k1 = dot(ro, pla.xyz);
+    float k2 = dot(rd, pla.xyz);
+    float t = (pla.w-k1)/k2;        
+    vec2 ab = (k2>0.0) ? vec2( t, 1e20 ) : vec2( -1e20, t );
+    return Intersection( vec4(ab.x, -pla.xyz), vec4(ab.y, pla.xyz) );
+}
+
+
 // The MIT License
 // Copyright © 2022 Inigo Quilez
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
