a571a3751fb68d439149d3dbcde5b6b95c0ccc38
jb <jon.baker1222@gmail.com>
2023-10-04 23:15:53 -0500

    3d discrete gaussian kernel + normalize for 3d

--- src/projects/Physarum/3D/shaders/diffuseAndDecay.cs.glsl
+++ src/projects/Physarum/3D/shaders/diffuseAndDecay.cs.glsl
@@ -1,24 +1,45 @@
 #version 430 core
-layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout( local_size_x = 8, local_size_y = 8, local_size_z = 8 ) in;
 
-layout( binding = 1, r32ui ) uniform uimage2D previous;
-layout( binding = 2, r32ui ) uniform uimage2D current;
+layout( binding = 1, r32ui ) uniform uimage3D previous;
+layout( binding = 2, r32ui ) uniform uimage3D current;
 
 uniform float decayFactor;
 
 void main() {
 	// gaussian kernel
-	ivec2 pos = ivec2( gl_GlobalInvocationID.xy );
+	ivec3 pos = ivec3( gl_GlobalInvocationID.xyz );
 	uint g = (
-		1 * imageLoad( previous, pos + ivec2( -1, -1 ) ).r +
-		1 * imageLoad( previous, pos + ivec2( -1,  1 ) ).r +
-		1 * imageLoad( previous, pos + ivec2(  1, -1 ) ).r +
-		1 * imageLoad( previous, pos + ivec2(  1,  1 ) ).r +
-		2 * imageLoad( previous, pos + ivec2(  0,  1 ) ).r +
-		2 * imageLoad( previous, pos + ivec2(  0, -1 ) ).r +
-		2 * imageLoad( previous, pos + ivec2(  1,  0 ) ).r +
-		2 * imageLoad( previous, pos + ivec2( -1,  0 ) ).r +
-		4 * imageLoad( previous, pos + ivec2(  0,  0 ) ).r ) / 16;
+		1 * imageLoad( previous, pos + ivec3( -1, -1, -1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3(  1, -1, -1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3( -1,  1, -1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3(  1,  1, -1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3( -1, -1,  1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3(  1, -1,  1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3( -1,  1,  1 ) ).r +
+		1 * imageLoad( previous, pos + ivec3(  1,  1,  1 ) ).r +
+
+		2 * imageLoad( previous, pos + ivec3(  1,  0, -1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  0, -1, -1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  0,  1, -1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3( -1,  0, -1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  1,  0,  1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  0, -1,  1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  0,  1,  1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3( -1,  0,  1 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  1,  1,  0 ) ).r +
+		2 * imageLoad( previous, pos + ivec3(  1, -1,  0 ) ).r +
+		2 * imageLoad( previous, pos + ivec3( -1,  1,  0 ) ).r +
+		2 * imageLoad( previous, pos + ivec3( -1, -1,  0 ) ).r +
+
+		4 * imageLoad( previous, pos + ivec3( -1,  0,  0 ) ).r +
+		4 * imageLoad( previous, pos + ivec3(  1,  0,  0 ) ).r +
+		4 * imageLoad( previous, pos + ivec3(  0, -1,  0 ) ).r +
+		4 * imageLoad( previous, pos + ivec3(  0,  1,  0 ) ).r +
+		4 * imageLoad( previous, pos + ivec3(  0,  0, -1 ) ).r +
+		4 * imageLoad( previous, pos + ivec3(  0,  0,  1 ) ).r +
+
+		8 * imageLoad( previous, pos + ivec3(  0,  0,  0 ) ).r ) / 64;
 
 	imageStore( current, pos, uvec4( uint( decayFactor * g ) ) );
 }
