08ed243995c0f7ab37509ef5daa5aacf06c63e7c
jb <jon.baker1222@gmail.com>
2023-10-26 05:33:28 -0500

    2.5D display working pretty well, not very good viewing angle

--- src/projects/Physarum/2.5D/shaders/bufferCopy.cs.glsl
+++ src/projects/Physarum/2.5D/shaders/bufferCopy.cs.glsl
@@ -17,6 +17,25 @@ float RemapRange ( const float value, const float iMin, const float iMax, const 
 	return ( oMin + ( ( oMax - oMin ) / ( iMax - iMin ) ) * ( value - iMin ) );
 }
 
+float blueNoiseRead ( ivec2 loc, int idx ) {
+	ivec2 wrappedLoc = loc % imageSize( blueNoiseTexture );
+	uvec4 sampleValue = imageLoad( blueNoiseTexture, wrappedLoc );
+	switch ( idx ) {
+	case 0:
+		return sampleValue.r / 255.0f;
+		break;
+	case 1:
+		return sampleValue.g / 255.0f;
+		break;
+	case 2:
+		return sampleValue.b / 255.0f;
+		break;
+	case 3:
+		return sampleValue.a / 255.0f;
+		break;
+	}
+}
+
 void main () {
 	// remapped uv
 	vec2 uv = ( vec2( gl_GlobalInvocationID.xy ) + vec2( 0.5f ) ) / resolution.xy;
@@ -27,24 +46,46 @@ void main () {
 
 	// box intersection
 	float tMin, tMax;
-	vec3 Origin = invBasis * vec3( scale * uv, -1.0f );
-	vec3 Direction = invBasis * vec3( 0.0f, 0.0f, 1.0f );
+	vec3 Origin = invBasis * vec3( scale * uv, -2.0f );
+	vec3 Direction = invBasis * normalize( vec3( uv * 0.1f, 2.0f ) );
 	const bool hit = Intersect( Origin, Direction, -blockSize / 2.0f, blockSize / 2.0f, tMin, tMax );
 
-	if ( hit ) {
-		// texture sample
+	if ( hit ) { // texture sample
+		// for trimming edges
+		const float epsilon = -0.003f; 
 		const vec3 hitpointMin = Origin + tMin * Direction;
 		const vec3 hitpointMax = Origin + tMax * Direction;
 		const vec3 blockUVMin = vec3(
-			RemapRange( hitpointMin.x, -blockSize.x / 2.0f, blockSize.x / 2.0f, 0.0f, 1.0f ),
-			RemapRange( hitpointMin.y, -blockSize.y / 2.0f, blockSize.y / 2.0f, 0.0f, 1.0f ),
-			RemapRange( hitpointMin.z, -blockSize.z / 2.0f, blockSize.z / 2.0f, 0.0f, 1.0f )
+			RemapRange( hitpointMin.x, -blockSize.x / 2.0f, blockSize.x / 2.0f, 0.0f - epsilon, 1.0f + epsilon ),
+			RemapRange( hitpointMin.y, -blockSize.y / 2.0f, blockSize.y / 2.0f, 0.0f - epsilon, 1.0f + epsilon ),
+			RemapRange( hitpointMin.z, -blockSize.z / 2.0f, blockSize.z / 2.0f, 0.0f - epsilon, 1.0f + epsilon )
 		);
 
-		float result = texture( continuum, blockUVMin.xy ).r / 1000000.0f;
-		const float thickness = abs( tMin - tMax );
-		const vec3 final = brightness * result * color + vec3( 0.01f );
+		const vec3 blockUVMax = vec3(
+			RemapRange( hitpointMax.x, -blockSize.x / 2.0f, blockSize.x / 2.0f, 0.0f - epsilon, 1.0f + epsilon ),
+			RemapRange( hitpointMax.y, -blockSize.y / 2.0f, blockSize.y / 2.0f, 0.0f - epsilon, 1.0f + epsilon ),
+			RemapRange( hitpointMax.z, -blockSize.z / 2.0f, blockSize.z / 2.0f, 0.0f - epsilon, 1.0f + epsilon )
+		);
 
+		const float thickness = abs( tMin - tMax );
+		const float stepSize = max( thickness / 10.0f, 0.001f );
+		const vec3 displacement = normalize( blockUVMax - blockUVMin );
+
+		const int numSteps = 700;
+		int iterationsToHit = 0;
+		vec3 samplePoint = blockUVMin + blueNoiseRead( ivec2( gl_GlobalInvocationID.xy ), 0 ) * 0.01f;
+		vec3 final = vec3( 0.01f );
+		for ( ; iterationsToHit < numSteps; iterationsToHit++ ) {
+			samplePoint += stepSize * displacement;
+			float result = brightness * ( texture( continuum, samplePoint.xy ).r / 1000000.0f );
+			if ( result > ( ( samplePoint.z - 0.5f ) * 2.0f ) ) {
+				break;
+			}
+			// vec3 iterationColor = brightness * result * color + vec3( 0.003f );
+			vec3 iterationColor = color;
+			final = mix( final, iterationColor, 0.01f );
+		}
+		final = mix( final, brightness * ( texture( continuum, samplePoint.xy ).r / 1000000.0f ) * color + vec3( 0.003f ), 0.2f );
 		imageStore( accumulatorTexture, ivec2( gl_GlobalInvocationID.xy ), vec4( final, 1.0f ) );
 	} else {
 		imageStore( accumulatorTexture, ivec2( gl_GlobalInvocationID.xy ), vec4( 0.0, 0.0f, 0.0f, 1.0f ) );
