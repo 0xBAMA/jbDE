44da0e51c5421edd541ad6bec676075d67dd57ad
jb <jon.baker1222@gmail.com>
2023-11-02 01:54:46 -0500

    wood material for the grid

--- src/projects/Aquaria/shaders/precompute.cs.glsl
+++ src/projects/Aquaria/shaders/precompute.cs.glsl
@@ -109,6 +109,97 @@ float perlinfbm( vec3 p, float freq, int octaves ) {
 // 	}
 // }
 
+// ==============================================================================================
+// procedural wood + support funcs from dean the coder https://www.shadertoy.com/view/mdy3R1
+///////////////////////////////////////////////////////////////////////////////
+#define sat(x) clamp(x,0.,1.)
+#define S(a,b,c) smoothstep(a,b,c)
+#define S01(a) S(0.,1.,a)
+
+float sum2(vec2 v) { return dot(v, vec2(1)); }
+float h31(vec3 p3) {
+	p3 = fract(p3 * .1031);
+	p3 += dot(p3, p3.yzx + 333.3456);
+	return fract(sum2(p3.xy) * p3.z);
+}
+
+float h21(vec2 p) { return h31(p.xyx); }
+float n31(vec3 p) {
+	const vec3 s = vec3(7, 157, 113);
+	// Thanks Shane - https://www.shadertoy.com/view/lstGRB
+	vec3 ip = floor(p);
+	p = fract(p);
+	p = p * p * (3. - 2. * p);
+	vec4 h = vec4(0, s.yz, sum2(s.yz)) + dot(ip, s);
+	h = mix(fract(sin(h) * 43758.545), fract(sin(h + s.x) * 43758.545), p.x);
+	h.xy = mix(h.xz, h.yw, p.y);
+	return mix(h.x, h.y, p.z);
+}
+// roughness: (0.0, 1.0], default: 0.5
+// Returns unsigned noise [0.0, 1.0]
+float fbm(vec3 p, int octaves, float roughness) {
+	float sum = 0.,
+	      amp = 1.,
+	      tot = 0.;
+	roughness = sat(roughness);
+	while (octaves-- > 0) {
+		sum += amp * n31(p);
+		tot += amp;
+		amp *= roughness;
+		p *= 2.;
+	}
+	return sum / tot;
+}
+vec3 randomPos(float seed) {
+	vec4 s = vec4(seed, 0, 1, 2);
+	return vec3(h21(s.xy), h21(s.xz), h21(s.xw)) * 1e2 + 1e2;
+}
+// Returns unsigned noise [0.0, 1.0]
+float fbmDistorted(vec3 p) {
+	p += (vec3(n31(p + randomPos(0.)), n31(p + randomPos(1.)), n31(p + randomPos(2.))) * 2. - 1.) * 1.12;
+	return fbm(p, 8, .5);
+}
+// vec3: detail(/octaves), dimension(/inverse contrast), lacunarity
+// Returns signed noise.
+float musgraveFbm(vec3 p, float octaves, float dimension, float lacunarity) {
+	float sum = 0.,
+	      amp = 1.,
+	      m = pow(lacunarity, -dimension);
+	while (octaves-- > 0.) {
+		float n = n31(p) * 2. - 1.;
+		sum += n * amp;
+		amp *= m;
+		p *= lacunarity;
+	}
+	return sum;
+}
+// Wave noise along X axis.
+vec3 waveFbmX(vec3 p) {
+	float n = p.x * 20.;
+	n += .4 * fbm(p * 3., 3, 3.);
+	return vec3(sin(n) * .5 + .5, p.yz);
+}
+///////////////////////////////////////////////////////////////////////////////
+// Math
+float remap01(float f, float in1, float in2) { return sat((f - in1) / (in2 - in1)); }
+///////////////////////////////////////////////////////////////////////////////
+// Wood material.
+vec3 matWood(vec3 p) {
+	float n1 = fbmDistorted(p * vec3(7.8, 1.17, 1.17));
+	n1 = mix(n1, 1., .2);
+	float n2 = mix(musgraveFbm(vec3(n1 * 4.6), 8., 0., 2.5), n1, .85),
+	      dirt = 1. - musgraveFbm(waveFbmX(p * vec3(.01, .15, .15)), 15., .26, 2.4) * .4;
+	float grain = 1. - S(.2, 1., musgraveFbm(p * vec3(500, 6, 1), 2., 2., 2.5)) * .2;
+	n2 *= dirt * grain;
+    
+    // The three vec3 values are the RGB wood colors - Tweak to suit.
+	return mix(mix(vec3(.03, .012, .003), vec3(.25, .11, .04), remap01(n2, .19, .56)), vec3(.52, .32, .19), remap01(n2, .56, 1.));
+}
+#undef sat
+#undef S
+#undef S01
+
+
 void main () {
 	// voxel location
 	const ivec3 writeLoc = ivec3( gl_GlobalInvocationID.xyz ) * 8 + offset;
@@ -118,12 +209,14 @@ void main () {
 	vec4 color = vec4( 0.0f );
 
 	// grid
-	const bool x = ( writeLoc.x + 10 ) % 55 < 4;
-	const bool y = ( writeLoc.y + 10 ) % 90 < 4;
-	const bool z = ( writeLoc.z + 15 ) % 32 < 3;
+	const bool x = ( writeLoc.x + 10 ) % 168 < 20;
+	const bool y = ( writeLoc.y + 10 ) % 128 < 16;
+	const bool z = ( writeLoc.z + 15 ) % 85 < 40;
 	if ( ( x && y ) || ( x && z ) || ( y && z ) ) {
-		if ( perlinfbm( pos / 168.0f, 2.0f, 3 ) < 0.125f ) {
-			color = vec4( vec3( 0.618f + perlinfbm( pos / 300.0f, 2.0f, 3 ) ), 1.0f );
+		if ( perlinfbm( pos / 500.0f, 2.0f, 6 ) < 0.11f ) {
+			// color = vec4( vec3( 0.618f + perlinfbm( pos / 300.0f, 2.0f, 3 ) ), 1.0f );
+			vec3 c = matWood( pos / 40.0f );
+			color = vec4( c, 1.0f + dot( c, vec3( 0.299f, 0.587f, 0.114f ) ) ); // luma to set alpha
 		}
 	}
 
