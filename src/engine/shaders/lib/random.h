ac1d8093fe41c10ec214fa210816d0986580133d
jb <jon.baker1222@gmail.com>
2023-11-04 11:34:28 -0500

    add header with the basic random utils I keep having to look up

---
+++ src/engine/shaders/lib/random.h
@@ -0,0 +1,28 @@
+// random utilites
+uint seed = 0;
+uint wangHash () {
+	seed = uint( seed ^ uint( 61 ) ) ^ uint( seed >> uint( 16 ) );
+	seed *= uint( 9 );
+	seed = seed ^ ( seed >> 4 );
+	seed *= uint( 0x27d4eb2d );
+	seed = seed ^ ( seed >> 15 );
+	return seed;
+}
+
+float normalizedRandomFloat () {
+	return float( wangHash() ) / 4294967296.0f;
+}
+
+const float pi = 3.14159265358979323846f;
+vec3 randomUnitVector () {
+	float z = normalizedRandomFloat() * 2.0f - 1.0f;
+	float a = normalizedRandomFloat() * 2.0f * pi;
+	float r = sqrt( 1.0f - z * z );
+	float x = r * cos( a );
+	float y = r * sin( a );
+	return vec3( x, y, z );
+}
+
+vec2 randomInUnitDisk () {
+	return randomUnitVector().xy;
+}
