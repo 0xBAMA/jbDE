4103660fd87b299b72984006cc3c7eb5ab9a31d4
jb <jon.baker1222@gmail.com>
2023-11-05 21:01:13 -0600

    integer power func w lut

--- src/engine/coreUtils/math.h
+++ src/engine/coreUtils/math.h
@@ -12,6 +12,80 @@ inline float RangeRemap ( float value, float inLow, float inHigh, float outLow, 
 	return outLow + ( value - inLow ) * ( outHigh - outLow ) / ( inHigh - inLow );
 }
 
+// fast integer power using a LUT - from https://gist.github.com/orlp/3551590
+inline int64_t intPow ( int64_t base, uint8_t exp ) {
+	static const uint8_t highest_bit_set[] = {
+		0, 1, 2, 2, 3, 3, 3, 3,
+		4, 4, 4, 4, 4, 4, 4, 4,
+		5, 5, 5, 5, 5, 5, 5, 5,
+		5, 5, 5, 5, 5, 5, 5, 5,
+		6, 6, 6, 6, 6, 6, 6, 6,
+		6, 6, 6, 6, 6, 6, 6, 6,
+		6, 6, 6, 6, 6, 6, 6, 6,
+		6, 6, 6, 6, 6, 6, 6, 255, // anything past 63 is a guaranteed overflow with base > 1
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+		255, 255, 255, 255, 255, 255, 255, 255,
+	};
+
+	int64_t result = 1;
+	switch ( highest_bit_set[ exp ] ) {
+	case 255: // we use 255 as an overflow marker and return 0 on overflow/underflow
+		if ( base == 1 ) {
+			return 1;
+		}
+		if ( base == -1 ) {
+			return 1 - 2 * ( exp & 1 );
+		}
+		return 0;
+	case 6:
+		if ( exp & 1 ) result *= base;
+		exp >>= 1;
+		base *= base;
+	case 5:
+		if ( exp & 1 ) result *= base;
+		exp >>= 1;
+		base *= base;
+	case 4:
+		if ( exp & 1 ) result *= base;
+		exp >>= 1;
+		base *= base;
+	case 3:
+		if ( exp & 1 ) result *= base;
+		exp >>= 1;
+		base *= base;
+	case 2:
+		if ( exp & 1 ) result *= base;
+		exp >>= 1;
+		base *= base;
+	case 1:
+		if ( exp & 1 ) result *= base;
+	default:
+		return result;
+	}
+}
+
 // other curves? there were some things
 	// Bias and Gain Functions - https://arxiv.org/abs/2010.09714
 	// iq's Usful Functions - https://iquilezles.org/articles/functions/
