4873497227f315d9d0c3100544a5685fb6f5b4f3
jb <jon.baker1222@gmail.com>
2023-12-03 20:14:44 -0600

    add more intersectors

--- src/engine/shaders/lib/intersect.h
+++ src/engine/shaders/lib/intersect.h
@@ -1,4 +1,4 @@
-bool Intersect ( const vec3 rO, vec3 rD, in vec3 min, in vec3 max, out float tMin, out float tMax ) {
+bool IntersectAABB ( const vec3 rO, vec3 rD, in vec3 min, in vec3 max, out float tMin, out float tMax ) {
 // Intersect() code adapted from:
 //    Amy Williams, Steve Barrus, R. Keith Morley, and Peter Shirley
 //    "An Efficient and Robust Ray-Box Intersection Algorithm"
@@ -25,4 +25,30 @@ bool Intersect ( const vec3 rO, vec3 rD, in vec3 min, in vec3 max, out float tMi
 	if ( tZMin > tMin ) tMin = tZMin;
 	if ( tZMax < tMax ) tMax = tZMax;
 	return ( ( tMin < maxDistance ) && ( tMax > minDistance ) );
+}
+
+vec2 RaySphereIntersect ( vec3 r0, vec3 rd, vec3 s0, float sr ) {
+	// r0 is ray origin
+	// rd is ray direction
+	// s0 is sphere center
+	// sr is sphere radius
+	// return is the roots of the quadratic, includes negative hits
+	float a = dot( rd, rd );
+	vec3 s0_r0 = r0 - s0;
+	float b = 2.0f * dot( rd, s0_r0 );
+	float c = dot( s0_r0, s0_r0 ) - ( sr * sr );
+	float disc = b * b - 4.0f * a * c;
+	if ( disc < 0.0f ) {
+		return vec2( -1.0f, -1.0f );
+	} else {
+		return vec2( -b - sqrt( disc ), -b + sqrt( disc ) ) / ( 2.0f * a );
+	}
+}
+
+// disk: center c, normal n, radius r
+float diskIntersect ( in vec3 ro, in vec3 rd, vec3 c, vec3 n, float r ) {
+	vec3  o = ro - c;
+	float t = -dot( n, o ) / dot( rd, n );
+	vec3  q = o + rd * t;
+	return ( dot( q, q ) < r * r ) ? t : -1.0f;
 }
