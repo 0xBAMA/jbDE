eca0ecef0d597c28b84063834af20e3e4c4be5b6
jb <jon.baker1222@gmail.com>
2024-02-03 01:52:17 -0600

    add Ellipsoid basic code

---
+++ src/projects/Impostors/Ellipsoid/shaders/dummyDraw.cs.glsl
@@ -0,0 +1,14 @@
+#version 430
+layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
+
+layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
+layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
+
+void main () {
+	// pixel location
+	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
+	vec3 col = imageLoad( blueNoiseTexture, writeLoc % imageSize( blueNoiseTexture ) ).xyz / 255.0f;
+
+	// write the data to the image
+	imageStore( accumulatorTexture, writeLoc, vec4( col, 1.0f ) );
+}
