add7985a33c71ae7c150d0a9daf9567b81f25a9c
jb <jon.baker1222@gmail.com>
2024-02-06 21:50:21 -0600

    greeble geo visibility toggle

--- src/projects/PathTracing/Siren/main.cc
+++ src/projects/PathTracing/Siren/main.cc
@@ -78,6 +78,7 @@ struct sirenConfig_t {
 	float raymarchEpsilon;
 	float raymarchUnderstep;
 	bool skipRaymarch = false;
+	bool skipGreeble = false;
 
 	vec4 backgroundColor = vec4( vec3( 0.01618f ), 1.0f );	// background color for the image view
 	vec3 skylightColor = vec3( 0.0f );		// ray escape color
@@ -553,6 +554,7 @@ public:
 			ImGui::Text( " " );
 			ImGui::SeparatorText( " Raymarch Parameters " );
 			ImGui::Checkbox( "Skip Raymarch Geometry", &sirenConfig.skipRaymarch );
+			ImGui::Checkbox( "Skip Greeble Geometry", &sirenConfig.skipGreeble );
 			ImGui::SliderInt( "Max Steps", ( int * ) &sirenConfig.raymarchMaxSteps, 10, 500 );
 			ImGui::SliderInt( "Max Bounces", ( int * ) &sirenConfig.raymarchMaxBounces, 0, 50 );
 			ImGui::SliderFloat( "Max Distance", &sirenConfig.raymarchMaxDistance, 1.0f, 500.0f );
@@ -1101,6 +1103,7 @@ public:
 		glUniform1i( glGetUniformLocation( shader, "bokehMode" ),				sirenConfig.bokehMode );
 		glUniform1i( glGetUniformLocation( shader, "invertSky" ),				sirenConfig.invertSky ); // add to config, animation control
 		glUniform1i( glGetUniformLocation( shader, "skipRaymarch" ),			sirenConfig.skipRaymarch ); // add to config, animation control
+		glUniform1i( glGetUniformLocation( shader, "skipGreeble" ),				sirenConfig.skipGreeble ); // add to config, animation control
 		glUniform3fv( glGetUniformLocation( shader, "skylightColor" ), 1,		glm::value_ptr( sirenConfig.skylightColor ) );
 		glUniform3fv( glGetUniformLocation( shader, "basisX" ), 1,				glm::value_ptr( sirenConfig.basisX ) );
 		glUniform3fv( glGetUniformLocation( shader, "basisY" ), 1,				glm::value_ptr( sirenConfig.basisY ) );
