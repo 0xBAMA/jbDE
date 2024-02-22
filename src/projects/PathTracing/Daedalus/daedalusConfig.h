cd2cf4772f94441d6ef53ba276bdf2a36b1395c8
jb <jon.baker1222@gmail.com>
2024-02-21 19:42:25 -0600

    reorganize config, add tiledispenser

--- src/projects/PathTracing/Daedalus/daedalusConfig.h
+++ src/projects/PathTracing/Daedalus/daedalusConfig.h
@@ -1,14 +1,36 @@
+#include "tileDispenser.h"
+
 struct daedalusConfig_t {
 
-	// tabbed interface for configuring scene/rendering parameters
-	bool showConfigWindow = true;
+	daedalusConfig_t() {
+		// configuring the accumulator
+		targetWidth = 640;
+		targetHeight = 360;
+		tileSize = 16;
+
+		// initialize the tile dispenser
+		tiles = tileDispenser( tileSize, targetWidth, targetHeight );
+
+		// intialize the main view
+		showConfigWindow = true;
+		outputZoom = 1.0f;
+		outputOffset = -2.0f * vec2( targetWidth, targetHeight );
+
+		// load a config file and shit?
+			// tbd, that could be a nice way to handle this
+	}
 
 	// sizing for accumulators, etc
-	uint32_t targetWidth = 640;
-	uint32_t targetHeight = 360;
+	uint32_t targetWidth;
+	uint32_t targetHeight;
+	uint32_t tileSize;
+	tileDispenser tiles;
+
+	// tabbed interface for configuring scene/rendering parameters
+	bool showConfigWindow;
 
 	// grid zoom
-	float outputZoom = 1.0f;
-	vec2 outputOffset = vec2( 0.0f );
+	float outputZoom;
+	vec2 outputOffset;
 
 };
