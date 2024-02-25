30a9c296d2464ca256229b1e5a285f1ea4e1d5ed
jb <jon.baker1222@gmail.com>
2024-02-25 01:50:32 -0600

    add old camera code, plans for tomorrow

--- src/projects/PathTracing/Daedalus/shaders/pathtrace.cs.glsl
+++ src/projects/PathTracing/Daedalus/shaders/pathtrace.cs.glsl
@@ -12,6 +12,7 @@ uniform int wangSeed;
 #include "random.h"		// rng + remapping utilities
 #include "twigl.glsl"	// noise, some basic math utils
 #include "hg_sdf.glsl"	// SDF modeling + booleans, etc
+#include "mathUtils.h"	// couple random math utilities
 
 bool BoundsCheck( in ivec2 loc ) {
 	const ivec2 b = ivec2( imageSize( accumulatorColor ) ).xy;
@@ -69,10 +70,112 @@ vec2 SubpixelOffset () {
 //=============================================================================================================================
 //=============================================================================================================================
 
+
+#define NORMAL		0
+#define SPHERICAL	1
+#define SPHERICAL2	2
+#define SPHEREBUG	3
+#define SIMPLEORTHO	4
+#define ORTHO		5
+
+uniform vec3 viewerPosition;
+uniform vec3 basisX;
+uniform vec3 basisY;
+uniform vec3 basisZ;
+
+uniform float uvScalar;
+
+uniform bool thinLensEnable;
+uniform float thinLensFocusDistance;
+uniform float thinLensJitterRadius;
+
 struct ray_t {
 	vec3 origin;
 	vec3 direction;
 };
+
+ray getCameraRayForUV ( vec2 uv ) { // switchable cameras ( fisheye, etc ) - Assumes -1..1 range on x and y
+	const float aspectRatio = float( imageSize( accumulatorColor ).x ) / float( imageSize( accumulatorColor ).y );
+
+	// compound eye hex vision
+	// https://www.shadertoy.com/view/4lfcR7
+
+	ray r;
+	r.origin	= vec3( 0.0f );
+	r.direction	= vec3( 0.0f );
+
+	// apply uvScalar here, probably
+
+	switch ( cameraType ) {
+	case NORMAL: {
+		r.origin = viewerPosition;
+		r.direction = normalize( aspectRatio * uv.x * basisX + uv.y * basisY + ( 1.0f / FoV ) * basisZ );
+		break;
+	}
+
+	case SPHERICAL: {
+		uv *= uvScalar;
+		uv.y /= aspectRatio;
+		uv.x -= 0.05f;
+		uv = vec2( atan( uv.y, uv.x ) + 0.5f, ( length( uv ) + 0.5f ) * acos( -1.0f ) );
+		vec3 baseVec = normalize( vec3( cos( uv.y ) * cos( uv.x ), sin( uv.y ), cos( uv.y ) * sin( uv.x ) ) );
+		// derived via experimentation, pretty close to matching the orientation of the normal camera
+		baseVec = Rotate3D( PI / 2.0f, vec3( 2.5f, 0.4f, 1.0f ) ) * baseVec;
+		r.origin = viewerPosition;
+		r.direction = normalize( -baseVec.x * basisX + baseVec.y * basisY + ( 1.0f / FoV ) * baseVec.z * basisZ );
+		break;
+	}
+
+	case SPHERICAL2: {
+		uv *= uvScalar;
+		mat3 rotY = Rotate3D( uv.y, basisX ); // rotate up/down by uv.y
+		mat3 rotX = Rotate3D( uv.x, basisY ); // rotate left/right by uv.x
+		r.origin = viewerPosition;
+		r.direction = rotX * rotY * basisZ;
+		break;
+	}
+
+	case SPHEREBUG: {
+		uv *= uvScalar;
+		uv.y /= aspectRatio;
+		uv = vec2( atan( uv.y, uv.x ) + 0.5f, ( length( uv ) + 0.5f ) * acos( -1.0f ) );
+		vec3 baseVec = normalize( vec3( cos( uv.y ) * cos( uv.x ), sin( uv.y ), cos( uv.y ) * sin( uv.x ) ) );
+		r.origin = viewerPosition;
+		r.direction = normalize( -baseVec.x * basisX + baseVec.y * basisY + ( 1.0f / FoV ) * baseVec.z * basisZ );
+		break;
+	}
+
+	case SIMPLEORTHO: { // basically a long focal length, not quite orthographic
+	// this isn't correct - need to adjust ray origin with basisX and basisY, and set ray direction equal to basisZ
+		uv.y /= aspectRatio;
+		vec3 baseVec = vec3( uv * FoV, 1.0f );
+		r.origin = viewerPosition;
+		r.direction = normalize( basisX * baseVec.x + basisY * baseVec.y + basisZ * baseVec.z );
+		break;
+	}
+
+	case ORTHO: {
+		uv.y /= aspectRatio;
+		r.origin = viewerPosition + basisX * FoV * uv.x + basisY * FoV * uv.y;
+		r.direction = basisZ;
+		break;
+	}
+
+	default:
+		break;
+	}
+
+	if ( thinLensEnable || cameraType == SPHEREBUG ) { // or we want that fucked up split sphere behavior... sphericalFucked, something
+		// thin lens adjustment
+		vec3 focuspoint = r.origin + ( ( r.direction * thinLensFocusDistance ) / dot( r.direction, basisZ ) );
+		vec2 diskOffset = thinLensJitterRadius * GetBokehOffset( bokehMode );
+		r.origin += diskOffset.x * basisX + diskOffset.y * basisY;
+		r.direction = normalize( focuspoint - r.origin );
+	}
+
+	return r;
+}
+
 
 //=============================================================================================================================
 //=============================================================================================================================
