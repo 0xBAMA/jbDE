eabc8df636a267c8e98cae3bf08051d7cfe3c7b6
jb <jon.baker1222@gmail.com>
2024-03-30 18:39:40 -0500

    cancel some terms, simplify ray-sphere

--- src/projects/Impostors/Chorizo/shaders/bbox.fs.glsl
+++ src/projects/Impostors/Chorizo/shaders/bbox.fs.glsl
@@ -16,15 +16,14 @@ vec2 RaySphereIntersect ( vec3 r0, vec3 rd, vec3 s0, float sr ) {
 	// s0 is sphere center
 	// sr is sphere radius
 	// return is the roots of the quadratic, includes negative hits
-	float a = dot( rd, rd );
-	vec3 s0_r0 = r0 - s0;
-	float b = 2.0f * dot( rd, s0_r0 );
-	float c = dot( s0_r0, s0_r0 ) - ( sr * sr );
-	float disc = b * b - 4.0f * a * c;
+	r0 = r0 - s0;
+	float b = dot( rd, r0 );
+	float c = dot( r0, r0 ) - ( sr * sr );
+	float disc = b * b - c;
 	if ( disc < 0.0f ) {
 		return vec2( -1.0f, -1.0f );
 	} else {
-		return vec2( -b - sqrt( disc ), -b + sqrt( disc ) ) / ( 2.0f * a );
+		return vec2( -b - sqrt( disc ), -b + sqrt( disc ) );
 	}
 }
 
@@ -47,7 +46,7 @@ void main() {
 	const mat4 inverseTransform = inverse( transform );
 	const vec3 eyeVectorToFragment = vofiPosition - eyePosition;
 	const vec3 rayOrigin = ( inverseTransform * vec4( eyePosition, 1.0f ) ).xyz;
-	const vec3 rayDirection = ( inverseTransform * vec4( eyeVectorToFragment, 0.0f ) ).xyz;
+	const vec3 rayDirection = normalize( ( inverseTransform * vec4( eyeVectorToFragment, 0.0f ) ).xyz );
 	const vec3 sphereCenter = ( inverseTransform * vec4( vec3( 0.0f ), 0.0f ) ).xyz;
 
 	vec2 result = RaySphereIntersect( rayOrigin, rayDirection, sphereCenter, 1.0f );
