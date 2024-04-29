32b1aa97f6fc49fec44c89ad276469165ff96797
jb <jon.baker1222@gmail.com>
2024-04-29 18:59:09 -0500

    fix types on id textures

--- src/projects/Impostors/Chorizo/shaders/draw.cs.glsl
+++ src/projects/Impostors/Chorizo/shaders/draw.cs.glsl
@@ -4,8 +4,8 @@ layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
 layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
 layout( binding = 1, rgba16f ) uniform image2D normals;
 layout( binding = 2, rgba16f ) uniform image2D normalsBack;
-layout( binding = 3, r32ui ) uniform uimage2D primitiveID;
-layout( binding = 4, r32ui ) uniform uimage2D primitiveIDBack;
+layout( binding = 3, rg32ui ) uniform uimage2D primitiveID;
+layout( binding = 4, rg32ui ) uniform uimage2D primitiveIDBack;
 layout( binding = 5, rgba16f ) uniform image2D accumulatorTexture;
 
 uniform float time;
