6a0652942d4fbf599e93480dae58fef1eebcc4dc
jb <jon.baker1222@gmail.com>
2024-06-03 01:59:10 -0500

    trying hsv for vectorscope

--- src/projects/PathTracing/Daedalus/shaders/vectorscopeComposite.cs.glsl
+++ src/projects/PathTracing/Daedalus/shaders/vectorscopeComposite.cs.glsl
@@ -10,8 +10,22 @@ layout( rgba8 ) uniform image2D compositedResult;
 // buffer for max pixel count
 layout( binding = 5, std430 ) buffer perColumnMins { uint maxCount; };
 
-// rgb <-> oklch
-#include "oklabConversions.h.glsl"
+vec3 rgb2hsv( vec3 c ) {
+	vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+	vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+	vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+	float d = q.x - min(q.w, q.y);
+	float e = 1.0e-10;
+	return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv2rgb( in vec3 c ) {
+	vec3 rgb = clamp( abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),6.0)-3.0)-1.0, 0.0, 1.0 );
+	// rgb = rgb*rgb*(3.0-2.0*rgb); // cubic smoothing
+	return c.z * mix( vec3(1.0), rgb, c.y);
+}
+
 
 void main () {
 	const ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
@@ -24,10 +38,12 @@ void main () {
 		// smoothstep for edge falloff
 
 		// find the pixel count value, normalize by dividing by maxCount
-		const float value = float( imageLoad( vectorscopeImage, loc ).r ) / float( maxCount );
+		const float value = 2.0f * float( imageLoad( vectorscopeImage, loc ).r ) / float( maxCount );
 
-		// find the color for this pixel, from position -> lch color, set l=value -> rgb
-		color.rgb = oklch_to_srgb( vec3( value, radius, atan( normalizedSpace.x, normalizedSpace.y ) ) );
+		// find the color for this pixel, from position -> hsv color, set l=value -> rgb
+		color.rgb = hsv2rgb( vec3( atan( normalizedSpace.x, normalizedSpace.y ) / 6.28f, radius * 2.0f, value ) );
+	} else {
+		color.a = smoothstep( 0.5f, 0.4f, radius );
 	}
 
 	imageStore( compositedResult, loc, color );
