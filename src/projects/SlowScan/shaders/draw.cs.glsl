f96a0f3a2294c7825ed11eb6e794491484ed17a8
jb <jon.baker1222@gmail.com>
2024-06-29 22:39:47 -0500

    add waveform and fft magnitude display to shader

--- src/projects/SlowScan/shaders/draw.cs.glsl
+++ src/projects/SlowScan/shaders/draw.cs.glsl
@@ -6,6 +6,7 @@ layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
 
 uniform float time;
 
+uniform int dataSize;
 layout( binding = 0, std430 ) readonly buffer signalData	{ float inputData[]; };
 layout( binding = 1, std430 ) readonly buffer fftData		{ float outputData[]; };
 
@@ -13,15 +14,34 @@ layout( binding = 1, std430 ) readonly buffer fftData		{ float outputData[]; };
 
 void main () {
 	// pixel location
-	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
+	const ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
+	const ivec2 sampleLoc = writeLoc; // make it bigger
+
+	// pull data from the buffer
+	const float signalValue = inputData[ sampleLoc.x ];
+
+	// dc offset is in fftData[ 0 ], also remember that real signals are mirrored, here, so we take the top half
+		// note also that the lowest frequencies are towards the top end of the array, so it's inverted
+	const int idx = sampleLoc.x / 2;
+	const float fftValue = outputData[ idx ];
 
 	vec3 col = vec3( 0.0f );
-
-	if ( writeLoc.y < 100 ) {
-		float signalValue = inputData[ writeLoc.x ];
-		col = vec3( saturate( signalValue ), 0.0f, saturate( -signalValue ) );
-	// } else if () {
-
+	if ( sampleLoc.x < dataSize ) {
+		if ( sampleLoc.y < 100 ) { // polarized signal display
+			col = vec3( saturate( signalValue ), 0.0f, saturate( -signalValue ) );
+		} else if ( sampleLoc.y < 200 ) { // waveform graph
+			const float graphY = RangeRemapValue( float( sampleLoc.y ), 100.0f, 200.0f, 1.0f, -1.0f );
+			if ( abs( graphY - signalValue ) < 0.05f ) {
+				col = vec3( 1.0f );
+			} else if ( abs( graphY ) < 0.025f ) { // axis marker
+				col = vec3( 0.1618f );
+			}
+		} else if ( sampleLoc.y < 500 ) { // show the fft data
+			const float graphY = RangeRemapValue( float( sampleLoc.y ), 200.0f, 500.0f, 200.0f, 0.0f );
+			if ( graphY < fftValue ) {
+				col = vec3( 1.0f );
+			}
+		}
 	}
 
 	// write the data to the image
