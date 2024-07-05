3168283ee4c751161dccd546c4488ff3004570bd
jb <jon.baker1222@gmail.com>
2024-07-05 16:59:59 -0500

    swap out for ray-box

--- src/projects/CellularAutomata/CAHistory3D/shaders/draw.cs.glsl
+++ src/projects/CellularAutomata/CAHistory3D/shaders/draw.cs.glsl
@@ -9,16 +9,31 @@ layout( binding = 2, r8ui ) uniform uimage3D CAStateBuffer;
 
 uniform vec3 viewerPosition;
 
-float planeTest( in vec3 ro, in vec3 rd, in vec3 planeNormal, in float planeDist ) {
-	const float maxDist = 1e10f;
-	const vec2 distBound = vec2( 0.0001f, maxDist );
+const float maxDist = 1e10f;
+const vec2 distBound = vec2( 0.0001f, maxDist );
+float box( in vec3 ro, in vec3 rd, in vec3 boxSize, in vec3 boxPosition ) {
+	ro -= boxPosition;
 
-	float a = dot( rd, planeNormal );
-	float d = -( dot( ro, planeNormal ) + planeDist ) / a;
-	if ( a > 0.0f || d < distBound.x || d > distBound.y ) {
+	vec3 m = sign( rd ) / max( abs( rd ), 1e-8 );
+	vec3 n = m * ro;
+	vec3 k = abs( m ) * boxSize;
+
+	vec3 t1 = -n - k;
+	vec3 t2 = -n + k;
+
+	float tN = max( max( t1.x, t1.y ), t1.z );
+	float tF = min( min( t2.x, t2.y ), t2.z );
+
+	if ( tN > tF || tF <= 0.0f ) {
 		return maxDist;
 	} else {
-		return d;
+		if ( tN >= distBound.x && tN <= distBound.y ) {
+			return tN;
+		} else if ( tF >= distBound.x && tF <= distBound.y ) {
+			return tF;
+		} else {
+			return maxDist;
+		}
 	}
 }
 
@@ -26,9 +41,8 @@ void main () {
 	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
 	vec3 color = vec3( 0.0f );
 
-	const vec2 iS = imageSize( accumulatorTexture ).xy;
-
 	// generate eye ray
+	const vec2 iS = imageSize( accumulatorTexture ).xy;
 	const vec2 uv = vec2( writeLoc + 0.5f ) / iS;
 	const vec2 p = ( uv * 2.0f - 1.0f ) * vec2( iS.x / iS.y, 1.0f );
 
@@ -40,14 +54,14 @@ void main () {
 	const vec3 location = viewerPosition + p.x * left + p.y * up;
 
 	// ray-plane test
-	const float d = planeTest( location, forwards, vec3( 0.0f, 0.0f, 1.0f ), 0.0f );
+	const float d = box( location, forwards, vec3( 0.75f, 0.75f, 10.0f ), vec3( 0.0f, 0.0f, -10.0f ) );
 	const vec3 hitPosition = location + forwards * d;
 
 	// dda traversal, from intersection point
 
 	// write the data to the image
-	if ( abs( hitPosition.x ) < 0.5f && abs( hitPosition.y ) < 0.5f ) {
-		color = vec3( d ) * vec3( 1.0f, 0.0f, 0.0f );
+	if ( d < maxDist ) {
+		color = vec3( d / 5.0f );
 	}
 
 	imageStore( accumulatorTexture, writeLoc, vec4( color, 1.0f ) );
