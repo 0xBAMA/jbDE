ca17b6c987e76cf455d893b5083f459339501c7f
jb <jon.baker1222@gmail.com>
2024-07-09 16:06:37 -0500

    color blending across generations

--- src/projects/PathTracing/Daedalus/shaders/GoL.cs.glsl
+++ src/projects/PathTracing/Daedalus/shaders/GoL.cs.glsl
@@ -4,7 +4,17 @@ layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
 uniform int previousSlice;
 layout( rgba8ui ) uniform uimage3D ddatex;
 
-// vec3 color = vec3( 0.0f );
+vec3 colorAccumulate = vec3( 0.0f );
+float contributions = 0.0f;
+
+bool check ( ivec3 location ) {
+	uvec4 value = imageLoad( ddatex, location );
+	if ( value.a == 255 ) {
+		colorAccumulate += value.rgb;
+		contributions += 1.0f;
+	}
+	return ( value.a == 255 );
+}
 
 bool getState ( ivec3 location ) {
 	// read state from back buffer
@@ -12,15 +22,15 @@ bool getState ( ivec3 location ) {
 
 	// read neighborhood values from back buffer
 	uint count = 0;
-	count += ( ( imageLoad( ddatex, location + ivec3( -1, -1,  0 ) ).a ) == 255 ) ? 1 : 0;
-	count += ( ( imageLoad( ddatex, location + ivec3(  0, -1,  0 ) ).a ) == 255 ) ? 1 : 0;
-	count += ( ( imageLoad( ddatex, location + ivec3(  1, -1,  0 ) ).a ) == 255 ) ? 1 : 0;
-	count += ( ( imageLoad( ddatex, location + ivec3( -1,  0,  0 ) ).a ) == 255 ) ? 1 : 0;
+	count += check( location + ivec3( -1, -1,  0 ) ) ? 1 : 0;
+	count += check( location + ivec3(  0, -1,  0 ) ) ? 1 : 0;
+	count += check( location + ivec3(  1, -1,  0 ) ) ? 1 : 0;
+	count += check( location + ivec3( -1,  0,  0 ) ) ? 1 : 0;
 	// skip center pixel, already exists in previousState
-	count += ( ( imageLoad( ddatex, location + ivec3(  1,  0,  0 ) ).a ) == 255 ) ? 1 : 0;
-	count += ( ( imageLoad( ddatex, location + ivec3( -1,  1,  0 ) ).a ) == 255 ) ? 1 : 0;
-	count += ( ( imageLoad( ddatex, location + ivec3(  0,  1,  0 ) ).a ) == 255 ) ? 1 : 0;
-	count += ( ( imageLoad( ddatex, location + ivec3(  1,  1,  0 ) ).a ) == 255 ) ? 1 : 0;
+	count += check( location + ivec3(  1,  0,  0 ) ) ? 1 : 0;
+	count += check( location + ivec3( -1,  1,  0 ) ) ? 1 : 0;
+	count += check( location + ivec3(  0,  1,  0 ) ) ? 1 : 0;
+	count += check( location + ivec3(  1,  1,  0 ) ) ? 1 : 0;
 
 	// determine new state - Conway's Game of Life rules
 	return ( ( count == 2 || count == 3 ) && previousState == 1 ) || ( count == 3 && previousState == 0 );
@@ -41,6 +51,12 @@ void main () {
 	// 		state = 0;
 	// 	}
 	// }
+
+	if ( contributions != 0.0f ) {
+		colorAccumulate /= contributions;
+		// color = uvec3( mix( vec3( color ), colorAccumulate, 0.1f ) );
+		color = uvec3( colorAccumulate ) + uvec3( 1 );
+	}
 
 	// write the data to the front buffer
 	imageStore( ddatex, ivec3( writeLoc, previousSlice + 1 ), uvec4( color, state ) );
