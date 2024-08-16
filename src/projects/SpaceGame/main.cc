73d52d0bf26bb5201f6b85cdc276d9d6a54485a1
jb <jon.baker1222@gmail.com>
2024-08-15 19:57:04 -0500

    add basics for spacegame

---
+++ src/projects/SpaceGame/main.cc
@@ -0,0 +1,151 @@
+#include "../../engine/engine.h"
+
+class engineDemo final : public engineBase { // sample derived from base engine class
+public:
+	engineDemo () { Init(); OnInit(); PostInit(); }
+	~engineDemo () { Quit(); }
+
+	void OnInit () {
+		ZoneScoped;
+		{
+			Block Start( "Additional User Init" );
+
+			// something to put some basic data in the accumulator texture - specific to the demo project
+			shaders[ "Dummy Draw" ] = computeShader( "./src/projects/EngineDemo/shaders/dummyDraw.cs.glsl" ).shaderHandle;
+
+
+		}
+
+	}
+
+	void HandleCustomEvents () {
+		// application specific controls
+		ZoneScoped; scopedTimer Start( "HandleCustomEvents" );
+
+		// new data into the input handler
+		inputHandler.update();
+
+	}
+
+	void ImguiPass () {
+		ZoneScoped;
+
+
+		// =============================================================
+		ImGui::Begin( "Window" );
+
+		ImGui::Text( "This is some text" );
+
+		if ( ImGui::Button( "This is a button" ) ) {
+			cout << "it says the n word when you press it" << endl;
+		}
+
+		ImGui::End();
+		// =============================================================
+
+
+		if ( tonemap.showTonemapWindow ) {
+			TonemapControlsWindow();
+		}
+
+		if ( showProfiler ) {
+			static ImGuiUtils::ProfilersWindow profilerWindow; // add new profiling data and render
+			profilerWindow.cpuGraph.LoadFrameData( &tasks_CPU[ 0 ], tasks_CPU.size() );
+			profilerWindow.gpuGraph.LoadFrameData( &tasks_GPU[ 0 ], tasks_GPU.size() );
+			profilerWindow.Render(); // GPU graph is presented on top, CPU on bottom
+		}
+
+		QuitConf( &quitConfirm ); // show quit confirm window, if triggered
+
+		if ( showDemoWindow ) ImGui::ShowDemoWindow( &showDemoWindow );
+	}
+
+	void DrawAPIGeometry () {
+		ZoneScoped; scopedTimer Start( "API Geometry" );
+		// draw some shit - need to add a hello triangle to this, so I have an easier starting point for raster stuff
+	}
+
+	void ComputePasses () {
+		ZoneScoped;
+
+		{ // dummy draw - draw something into accumulatorTexture
+			scopedTimer Start( "Drawing" );
+			bindSets[ "Drawing" ].apply();
+			glUseProgram( shaders[ "Dummy Draw" ] );
+			glUniform1f( glGetUniformLocation( shaders[ "Dummy Draw" ], "time" ), SDL_GetTicks() / 1600.0f );
+			glDispatchCompute( ( config.width + 15 ) / 16, ( config.height + 15 ) / 16, 1 );
+			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+
+		{ // postprocessing - shader for color grading ( color temp, contrast, gamma ... ) + tonemapping
+			scopedTimer Start( "Postprocess" );
+			bindSets[ "Postprocessing" ].apply();
+			glUseProgram( shaders[ "Tonemap" ] );
+			SendTonemappingParameters();
+			glDispatchCompute( ( config.width + 15 ) / 16, ( config.height + 15 ) / 16, 1 );
+			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+
+		// shader to apply dithering
+			// ...
+
+		// other postprocessing
+			// ...
+
+		{ // text rendering timestamp - required texture binds are handled internally
+			scopedTimer Start( "Text Rendering" );
+			textRenderer.Update( ImGui::GetIO().DeltaTime );
+			textRenderer.Draw( textureManager.Get( "Display Texture" ) );
+			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+
+		{ // show trident with current orientation
+			// scopedTimer Start( "Trident" );
+			// trident.Update( textureManager.Get( "Display Texture" ) );
+			// glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+	}
+
+	void OnUpdate () {
+		ZoneScoped; scopedTimer Start( "Update" );
+		// application-specific update code
+	}
+
+	void OnRender () {
+		ZoneScoped;
+		ClearColorAndDepth();		// if I just disable depth testing, this can disappear
+		DrawAPIGeometry();			// draw any API geometry desired
+		ComputePasses();			// multistage update of displayTexture
+		BlitToScreen();				// fullscreen triangle copying to the screen
+		{
+			scopedTimer Start( "ImGUI Pass" );
+			ImguiFrameStart();		// start the imgui frame
+			ImguiPass();			// do all the gui stuff
+			ImguiFrameEnd();		// finish imgui frame and put it in the framebuffer
+		}
+		window.Swap();				// show what has just been drawn to the back buffer
+	}
+
+	bool MainLoop () { // this is what's called from the loop in main
+		ZoneScoped;
+
+		// event handling
+		HandleTridentEvents();
+		HandleCustomEvents();
+		HandleQuitEvents();
+
+		// derived-class-specific functionality
+		OnUpdate();
+		OnRender();
+
+		FrameMark; // tells tracy that this is the end of a frame
+		PrepareProfilingData(); // get profiling data ready for next frame
+		return pQuit;
+	}
+};
+
+int main ( int argc, char *argv[] ) {
+	engineDemo engineInstance;
+	while( !engineInstance.MainLoop() );
+	return 0;
+}
