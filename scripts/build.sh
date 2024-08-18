1f03aa275f3434c29fdff82f3ce30996e781fe76
jb <jon.baker1222@gmail.com>
2024-08-18 00:39:55 -0500

    variable proc count for all make targets

--- scripts/build.sh
+++ scripts/build.sh
@@ -23,37 +23,37 @@ cd build
 # -j arg is max jobs + 1, here configured for 16 compilation threads
 
 # engine stuff
-# time make -j17 TracyClient
-# time make -j17 EngineDemo				# super basic demo
+# time make -j$(nproc) TracyClient
+# time make -j$(nproc) EngineDemo				# super basic demo
 
 # # child apps
 
 # # Simulations
-# time make -j17 Physarum2D				# agent-based sim
-# time make -j17 Physarum3D				# agent-based sim
+# time make -j$(nproc) Physarum2D				# agent-based sim
+# time make -j$(nproc) Physarum3D				# agent-based sim
 
 # # Pathtracing
-# time make -j17 Siren					# tile based async pathtracer
+# time make -j$(nproc) Siren					# tile based async pathtracer
 
 # # voxelspace variants
-# time make -j17 VoxelSpace				# voxelspace algorithm renderer
-# time make -j17 VoxelSpace_Erode		# visualizing the erosion update
-# time make -j17 VoxelSpace_Physarum	# visualizing the physarum sim
+# time make -j$(nproc) VoxelSpace				# voxelspace algorithm renderer
+# time make -j$(nproc) VoxelSpace_Erode		# visualizing the erosion update
+# time make -j$(nproc) VoxelSpace_Physarum	# visualizing the physarum sim
 
 # # softbody sim
-# time make -j17 SoftBodies				# soft body sim on the CPU
-# time make -j17 SoftBodiesGPU			# soft body sim on the GPU ( unfinished )
+# time make -j$(nproc) SoftBodies				# soft body sim on the CPU
+# time make -j$(nproc) SoftBodiesGPU			# soft body sim on the GPU ( unfinished )
 
 # # cellular automata variants
-# time make -j17 CABitPlanes			# parallel cellular automata
-# time make -j17 CAHistory				# cellular automata with history
-# time make -j17 CAColorSplit			# same as above, but different color distribution
-# time make -j17 GoL					# basic Game of Life
+# time make -j$(nproc) CABitPlanes			# parallel cellular automata
+# time make -j$(nproc) CAHistory				# cellular automata with history
+# time make -j$(nproc) CAColorSplit			# same as above, but different color distribution
+# time make -j$(nproc) GoL					# basic Game of Life
 
 # # vertexture point sprite sphere impostors variants
-# time make -j17 Vertexture				# point sprite sphere impostors
-# time make -j17 VertextureClassic		# Vertexture, with the trees
-# time make -j17 ProjectedFramebuffers	# projected framebuffers using the vertexture renderer
+# time make -j$(nproc) Vertexture				# point sprite sphere impostors
+# time make -j$(nproc) VertextureClassic		# Vertexture, with the trees
+# time make -j$(nproc) ProjectedFramebuffers	# projected framebuffers using the vertexture renderer
 
 # this runs all the targets in parallel, scaled with the number of processors on the machine ( e.g. -j4, -j16, etc )
 	# can use the above individual targets to enable / disable piecemeal
