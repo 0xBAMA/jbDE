ee2808f92a162cf7bd4fcff57bf9cc2ecc35ffc4
jb <jon.baker1222@gmail.com>
2024-09-01 00:37:49 -0500

    float string formatting utility

--- src/engine/coreUtils/math.h
+++ src/engine/coreUtils/math.h
@@ -7,6 +7,10 @@ inline std::string fixedWidthNumberString ( int32_t value, int32_t width = 5, co
 	return string( width - std::min( width, ( int ) to_string( value ).length() ), fill ) + to_string( value );
 }
 
+inline std::string fixedWidthNumberStringF ( float value, int32_t width = 5, const char fill = ' ' ) {
+	return string( width - std::min( width, ( int ) to_string( value ).length() ), fill ) + to_string( value );
+}
+
 // remap the value from [inLow..inHigh] to [outLow..outHigh]
 inline float RangeRemap ( float value, float inLow, float inHigh, float outLow, float outHigh ) {
 	return outLow + ( value - inLow ) * ( outHigh - outLow ) / ( inHigh - inLow );
