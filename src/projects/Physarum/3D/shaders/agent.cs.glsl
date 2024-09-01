a4fdbb4d9e9614e3594cdef195843df723a4849d
jb <jon.baker1222@gmail.com>
2024-09-01 12:12:39 -0500

    change out rng for header functions

--- src/projects/Physarum/3D/shaders/agent.cs.glsl
+++ src/projects/Physarum/3D/shaders/agent.cs.glsl
@@ -70,33 +70,7 @@ vec3 wrapPosition ( vec3 pos ) {
 
 // random utilites
 uniform int wangSeed;
-uint seed = 0;
-uint wangHash () {
-	seed = uint( seed ^ uint( 61 ) ) ^ uint( seed >> uint( 16 ) );
-	seed *= uint( 9 );
-	seed = seed ^ ( seed >> 4 );
-	seed *= uint( 0x27d4eb2d );
-	seed = seed ^ ( seed >> 15 );
-	return seed;
-}
-
-float normalizedRandomFloat () {
-	return float( wangHash() ) / 4294967296.0f;
-}
-
-const float pi = 3.14159265358979323846f;
-vec3 randomUnitVector () {
-	float z = normalizedRandomFloat() * 2.0f - 1.0f;
-	float a = normalizedRandomFloat() * 2.0f * pi;
-	float r = sqrt( 1.0f - z * z );
-	float x = r * cos( a );
-	float y = r * sin( a );
-	return vec3( x, y, z );
-}
-
-vec2 randomInUnitDisk () {
-	return randomUnitVector().xy;
-}
+#include "random.h"
 
 void main () {
 
@@ -132,7 +106,7 @@ void main () {
 		if ( length( weightedSum.xy ) < 0.1f ) {
 			// this is the random direction behavior
 			for ( int i = 0; i < 3; i++ ) {
-				updateRotation *= Rotate3D( normalizedRandomFloat(), randomUnitVector() );
+				updateRotation *= Rotate3D( NormalizedRandomFloat(), RandomUnitVector() );
 			}
 		} else {
 			float angle = atan( weightedSum.y, weightedSum.x );
