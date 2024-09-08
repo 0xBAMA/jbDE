652bf74211348a88a7633a832f794ea59b409f0c
jb <jon.baker1222@gmail.com>
2024-09-07 22:40:38 -0500

    apply stamp during second part of the update

--- src/projects/Physarum/3D/shaders/diffuseAndDecay.cs.glsl
+++ src/projects/Physarum/3D/shaders/diffuseAndDecay.cs.glsl
@@ -4,6 +4,15 @@ layout( local_size_x = 8, local_size_y = 8, local_size_z = 8 ) in;
 layout( binding = 1, r32ui ) uniform uimage3D previous;
 layout( binding = 2, r32ui ) uniform uimage3D current;
 
+layout( binding = 3, r8ui ) uniform uimage3D mask;
+
+uniform uint stampAmount;
+uniform bool applyStamp;
+
+bool maskValue ( ivec3 pos ) {
+	return ( imageLoad( mask, pos ).r > 0 );
+}
+
 uniform float decayFactor;
 
 void main() {
@@ -44,5 +53,11 @@ void main() {
 	// ...trying with no blur... since this post seems to skip it? or not... need to look at the code - https://denizbicer.com/202408-UnderstandingPhysarum.html
 	// uint g = imageLoad( previous, ( pos ) % ivec3( imageSize( previous ) ) ).r;
 
-	imageStore( current, pos, uvec4( uint( decayFactor * g ) ) );
+	// if ( maskValue( pos ) && applyStamp ) {
+	if ( maskValue( pos ) ) {
+		// imageStore( current, pos, uvec4( uint( stampAmount ) ) );
+		imageStore( current, pos, uvec4( max( uint( 400000 ), uint( decayFactor * g ) ) ) );
+	} else {
+		imageStore( current, pos, uvec4( uint( decayFactor * g ) ) );
+	}
 }
