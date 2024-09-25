40495742ddc32308d6cf59600bf02765e0faa8b6
jb <jon.baker1222@gmail.com>
2024-09-24 22:04:32 -0500

    add Icarus project

---
+++ src/projects/PathTracing/Icarus/main.cc
@@ -0,0 +1,130 @@
+#include "../../../engine/engine.h"
+
+class Icarus final : public engineBase { // sample derived from base engine class
+public:
+	Icarus () { Init(); OnInit(); PostInit(); }
+	~Icarus () { Quit(); }
+
+	void OnInit () {
+		ZoneScoped;
+		{
+			Block Start( "Additional User Init" );
+
+			// something to put some basic data in the accumulator texture - specific to the demo project
+			shaders[ "Draw" ] = computeShader( "./src/projects/PathTracing/Icarus/shaders/draw.cs.glsl" ).shaderHandle;
+
+
+		}
+	}
+
+	void HandleCustomEvents () {
+		// application specific controls
+		ZoneScoped; scopedTimer Start( "HandleCustomEvents" );
+
+		// get new data into the input handler
+		inputHandler.update();
+
+		// pass any signals into the terminal
+		terminal.update( inputHandler );
+	}
+
+	void ImguiPass () {
+		ZoneScoped;
+		if ( tonemap.showTonemapWindow ) {
+			TonemapControlsWindow();
+		}
+
+		if ( showProfiler ) {
+			static ImGuiUtils::ProfilersWindow profilerWindow; // add new profiling data and render
+			profilerWindow.cpuGraph.LoadFrameData( &tasks_CPU[ 0 ], tasks_CPU.size() );
+			profilerWindow.gpuGraph.LoadFrameData( &tasks_GPU[ 0 ], tasks_GPU.size() );
+			profilerWindow.Render(); // GPU graph is presented on top, CPU on bottom
+		}
+
+		QuitConf( &quitConfirm ); // show quit confirm window, if triggered
+
+		if ( showDemoWindow ) ImGui::ShowDemoWindow( &showDemoWindow );
+	}
+
+	void DrawAPIGeometry () {
+		ZoneScoped; scopedTimer Start( "API Geometry" );
+		// draw some shit - need to add a hello triangle to this, so I have an easier starting point for raster stuff
+	}
+
+	void ComputePasses () {
+		ZoneScoped;
+
+		{ // dummy draw - draw something into accumulatorTexture
+			scopedTimer Start( "Drawing" );
+			bindSets[ "Drawing" ].apply();
+			glUseProgram( shaders[ "Draw" ] );
+			glUniform1f( glGetUniformLocation( shaders[ "Draw" ], "time" ), SDL_GetTicks() / 1600.0f );
+			glDispatchCompute( ( config.width + 15 ) / 16, ( config.height + 15 ) / 16, 1 );
+			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+
+		{ // postprocessing - shader for color grading ( color temp, contrast, gamma ... ) + tonemapping
+			scopedTimer Start( "Postprocess" );
+			bindSets[ "Postprocessing" ].apply();
+			glUseProgram( shaders[ "Tonemap" ] );
+			SendTonemappingParameters();
+			glDispatchCompute( ( config.width + 15 ) / 16, ( config.height + 15 ) / 16, 1 );
+			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+
+		{ // text rendering timestamp - required texture binds are handled internally
+			scopedTimer Start( "Text Rendering" );
+			textRenderer.Clear();
+			textRenderer.Update( ImGui::GetIO().DeltaTime );
+
+			// show terminal, if active - check happens inside
+			textRenderer.drawTerminal( terminal );
+
+			// put the result on the display
+			textRenderer.Draw( textureManager.Get( "Display Texture" ) );
+			glMemoryBarrier( GL_SHADER_IMAGE_ACCESS_BARRIER_BIT );
+		}
+	}
+
+	void OnUpdate () {
+		ZoneScoped; scopedTimer Start( "Update" );
+		// application-specific update code
+	}
+
+	void OnRender () {
+		ZoneScoped;
+		ClearColorAndDepth();		// if I just disable depth testing, this can disappear
+		DrawAPIGeometry();			// draw any API geometry desired
+		ComputePasses();			// multistage update of displayTexture
+		BlitToScreen();				// fullscreen triangle copying to the screen
+		{
+			scopedTimer Start( "ImGUI Pass" );
+			ImguiFrameStart();		// start the imgui frame
+			ImguiPass();			// do all the gui stuff
+			ImguiFrameEnd();		// finish imgui frame and put it in the framebuffer
+		}
+		window.Swap();				// show what has just been drawn to the back buffer
+	}
+
+	bool MainLoop () { // this is what's called from the loop in main
+		ZoneScoped;
+
+		// event handling
+		HandleCustomEvents();
+		HandleQuitEvents();
+
+		// derived-class-specific functionality
+		OnUpdate();
+		OnRender();
+
+		FrameMark; // tells tracy that this is the end of a frame
+		PrepareProfilingData(); // get profiling data ready for next frame
+		return pQuit;
+	}
+};
+
+int main ( int argc, char *argv[] ) {
+	Icarus engineInstance;
+	while( !engineInstance.MainLoop() );
+	return 0;
+}
---
+++ src/projects/PathTracing/Icarus/shaders/draw.cs.glsl
@@ -0,0 +1,28 @@
+#version 430
+layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
+
+layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
+layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
+
+uniform float time;
+
+#include "spaceFillingCurves.h.glsl"
+#include "colorRamps.glsl.h"
+
+void main () {
+	// pixel location
+	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
+
+	// round up to next power of two
+	// ivec2 loc = ihilbert( xy2z( writeLoc ), int( ceil( log( min( writeLoc.x, writeLoc.y ) ) / log( 2 ) ) ) ); // linear ramps
+	ivec2 loc = z2xy( int( 1000.0f * time + hilbert( writeLoc, int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ) ) );
+	uint x = uint( loc.x ) % 512;
+	uint y = uint( loc.y ) % 512;
+	uint xor = ( x ^ y );
+	vec3 col = refPalette( xor / 511.0f, BLUESR ).rgb;
+
+	// vec3 col = refPalette( abs( sin( time + 0.000618f * hilbert( writeLoc / 5, int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ) ) ), BLUESR ).rgb;
+
+	// write the data to the image
+	imageStore( accumulatorTexture, writeLoc, vec4( col, 1.0f ) );
+}
