799d761c86bf6fa8316535c6b28ca75af38180c0
jb <jon.baker1222@gmail.com>
2024-09-25 17:22:19 -0500

    poking around with space filling curves

--- src/projects/PathTracing/Icarus/shaders/draw.cs.glsl
+++ src/projects/PathTracing/Icarus/shaders/draw.cs.glsl
@@ -13,15 +13,17 @@ void main () {
 	// pixel location
 	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
 
-	// round up to next power of two
-	// ivec2 loc = ihilbert( xy2z( writeLoc ), int( ceil( log( min( writeLoc.x, writeLoc.y ) ) / log( 2 ) ) ) ); // linear ramps
-	ivec2 loc = z2xy( int( 1000.0f * time + hilbert( writeLoc, int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ) ) );
-	uint x = uint( loc.x ) % 512;
-	uint y = uint( loc.y ) % 512;
-	uint xor = ( x ^ y );
-	vec3 col = refPalette( xor / 511.0f, BLUESR ).rgb;
+	// // round up to next power of two
+	// ivec2 loc = ihilbert( int( 1000.0f * time + xy2z( writeLoc ) ), int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ); // linear ramps
+	// // ivec2 loc = z2xy( int( 1000.0f * time + hilbert( writeLoc, int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ) ) );
+	// uint x = uint( loc.x ) % 512;
+	// uint y = uint( loc.y ) % 512;
+	// uint xor = ( x & y );
+	// vec3 col = refPalette( xor / 511.0f, BLUESR ).rgb;
 
-	// vec3 col = refPalette( abs( sin( time + 0.000618f * hilbert( writeLoc / 5, int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ) ) ), BLUESR ).rgb;
+	// // vec3 col = refPalette( abs( sin( time + 0.000618f * hilbert( writeLoc / 5, int( ceil( log( min( imageSize( accumulatorTexture ).x, imageSize( accumulatorTexture ).y ) ) / log( 2 ) ) ) ) ) ), BLUESR ).rgb;
+
+	vec3 col = vec3( 0.0f );
 
 	// write the data to the image
 	imageStore( accumulatorTexture, writeLoc, vec4( col, 1.0f ) );
