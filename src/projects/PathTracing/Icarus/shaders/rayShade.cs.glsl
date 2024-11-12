7ad0e11ae10d1bc7539b842bce52ad1e0bb6c9d8
jb <jon.baker1222@gmail.com>
2024-11-12 03:02:32 -0600

    some different approaches to scattering

--- src/projects/PathTracing/Icarus/shaders/rayShade.cs.glsl
+++ src/projects/PathTracing/Icarus/shaders/rayShade.cs.glsl
@@ -13,6 +13,20 @@ layout( binding = 1, r32ui ) uniform uimage2D gTally;
 layout( binding = 2, r32ui ) uniform uimage2D bTally;
 layout( binding = 3, r32ui ) uniform uimage2D count;
 
+vec3 anglePhong( float a, vec3 n ){
+	float r1 = NormalizedRandomFloat();
+	float r2 = NormalizedRandomFloat();
+	float t = pow( r2, 2.0f / ( 1.0f + a ) );
+	float x = cos( 2.0f * 3.14159f * r1 ) * sqrt( 1.0f - t );
+	float y = sin( 2.0f * 3.14159f * r1 ) * sqrt( 1.0f - t );
+	float z = sqrt( t );
+	vec3 W = ( abs( n.x ) > 0.99f ) ? vec3( 0.0f, 1.0f, 0.0f ) : vec3( 1.0f, 0.0f, 0.0f );
+	vec3 N = n;
+	vec3 T = normalize( cross( N, W ) );
+	vec3 B = cross( T, N );
+	return normalize( x * T + y * B + z * N );
+}
+
 void main () {
 	rayState_t myState = state[ gl_GlobalInvocationID.x ];
 
@@ -41,8 +55,15 @@ void main () {
 
 	} else if ( GetHitIntersector( myState ) == VOLUMEHIT ) {
 
+		// // from Nikolay
+		// vec3 other = vec3( NormalizedRandomFloat(), NormalizedRandomFloat(), NormalizedRandomFloat() ) * 2.0f - 1.0f;
+		// SetRayDirection( myState, normalize( other + anglePhong( 1800.0f, GetRayDirection( myState ) ) * 0.15f ) );
+
 		SetRayOrigin( myState, GetRayOrigin( myState ) + GetHitDistance( myState ) * GetRayDirection( myState ) );
+		// SetRayDirection( myState, normalize( 0.25f * GetRayDirection( myState ) + RandomUnitVector() ) );
 		SetRayDirection( myState, normalize( 2.0f * GetRayDirection( myState ) + RandomUnitVector() ) );
+		// SetRayDirection( myState, normalize( GetRayDirection( myState ) * RandomUnitVector() ) );
+		// SetRayDirection( myState, normalize( anglePhong( 100.0f, GetRayDirection( myState ) ) ) );
 		SetTransmission( myState, GetTransmission( myState ) * GetHitAlbedo( myState ) );
 
 	} else {
