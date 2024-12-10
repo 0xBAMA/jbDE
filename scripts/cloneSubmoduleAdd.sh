54c8b14abba3e79613a5bb15b81fecd1d3eac306
jb <jon.baker1222@gmail.com>
2024-12-10 16:38:40 -0600

    code to add submodule

--- scripts/cloneSubmoduleAdd.sh
+++ scripts/cloneSubmoduleAdd.sh
@@ -2,3 +2,4 @@
 git submodule add https://github.com/Auburn/FastNoise2.git src/utils/noise/FastNoise2
 git submodule add https://github.com/wolfpld/tracy.git src/utils/tracy
 git submodule add https://github.com/Raikiri/LegitProfiler.git src/utils/ImGUI/LegitProfiler
+git submodule add https://github.com/jbikker/tinybvh.git src/utils/tinybvh
