7515ab09cf6f22694b3f614d8a91d1edce8be079
jb <jon.baker1222@gmail.com>
2024-12-13 10:32:31 -0600

    decrease view position jitter, was creating artifacts

--- src/projects/PathTracing/Icarus/shaders/rayGenerate.cs.glsl
+++ src/projects/PathTracing/Icarus/shaders/rayGenerate.cs.glsl
@@ -113,7 +113,7 @@ cameraRay GetCameraRay ( vec2 uv ) {
 	}
 
 	// jittering the ray origin helps with some aliasing issues...
-	temp.origin += ( Blue( ivec2( gl_GlobalInvocationID.x, gl_GlobalInvocationID.x % 512 ) ).z / 255.0f ) * 0.1f * temp.direction;
+	temp.origin -= ( Blue( ivec2( gl_GlobalInvocationID.x, gl_GlobalInvocationID.x % 512 ) ).z / 255.0f ) * 0.01f * temp.direction;
 
 	// if the DoF is on, we do some additional calculation...
 	if ( DoFRadius != 0.0f && temp.direction != vec3( 0.0f ) ) {
