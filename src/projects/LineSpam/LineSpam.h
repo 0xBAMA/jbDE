ad81b84694a71a7e01cfa5ec9f3e35cefcea6248
jb <jon.baker1222@gmail.com>
2024-12-29 18:18:49 -0600

    texture setup

--- src/projects/LineSpam/LineSpam.h
+++ src/projects/LineSpam/LineSpam.h
@@ -31,25 +31,73 @@ struct LineSpamConfig_t {
 };
 
 void LineSpamConfig_t::CreateTextures() {
+	textureOptions_t opts;
+	opts.dataType		= GL_R32UI;
+	opts.width			= dimensions.x;
+	opts.height			= dimensions.y;
+	opts.minFilter		= GL_NEAREST;
+	opts.magFilter		= GL_NEAREST;
+	opts.textureType	= GL_TEXTURE_2D;
+	opts.wrap			= GL_CLAMP_TO_BORDER;
 
 	// depth buffer, uint
+	textureManager->Add( "Depth Buffer", opts );
+
 	// id buffer, uint
+	textureManager->Add( "ID Buffer", opts );
+
 	// r, g, b, count tallies, uint
+	textureManager->Add( "Red Tally Buffer", opts );
+	textureManager->Add( "Green Tally Buffer", opts );
+	textureManager->Add( "Blue Tally Buffer", opts );
+	textureManager->Add( "Sample Tally Buffer", opts );
 
-	// color buffer, LDR RGB
-
+	// color buffer, RGB color
+	opts.dataType		= GL_RGBA16F;
+	opts.magFilter		= GL_LINEAR;
+	textureManager->Add( "Composite Target", opts );
 }
 
 void LineSpamConfig_t::CompileShaders() {
-	// line passes
+	const string basePath = string( "./src/projects/LineSpam/shaders/" );
+	opaquePreZShader		= computeShader( basePath + "opaquePreZ.cs.glsl" ).shaderHandle;
+	opaqueDrawShader		= computeShader( basePath + "opaqueDraw.cs.glsl" ).shaderHandle;
+	transparentDrawShader	= computeShader( basePath + "transparentDraw.cs.glsl" ).shaderHandle;
+	compositeShader			= computeShader( basePath + "composite.cs.glsl" ).shaderHandle;
 }
 
 void LineSpamConfig_t::PrepLineBuffers() {
+	// unset flag
 	dirty = false;
-	// other buffer setup stuff...
+
+	// sorting the list of lines into opaque and transparent
+	vector< line > opaqueLines;
+	vector< line > transparentLines;
+
+	for ( auto& l : lines ) {
+		if ( l.color.a == 1.0f ) {
+			opaqueLines.push_back( l );
+		} else {
+			transparentLines.push_back( l );
+		}
+	}
+
+	// create the buffers
+
 }
 
-void LineSpamConfig_t::ClearPass() {}
-void LineSpamConfig_t::OpaquePass() {}
-void LineSpamConfig_t::TransparentPass() {}
-void LineSpamConfig_t::CompositePass() {}
+void LineSpamConfig_t::OpaquePass() {
+	// opaque pre z draw, establish closest depths per pixel
+	// opaque draw, depth equals, setting ID values for composite
+
+}
+
+void LineSpamConfig_t::TransparentPass() {
+	// transparent draw process
+
+}
+
+void LineSpamConfig_t::CompositePass() {
+	// this will also do the buffer clears, since it's the only consumer of the other buffer info
+
+}
