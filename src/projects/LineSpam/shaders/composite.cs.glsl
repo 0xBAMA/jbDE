00f6410b3f38125bf48ff93410674e23612b86cf
jb <jon.baker1222@gmail.com>
2024-12-30 04:40:27 -0600

    transparent line drawing working

--- src/projects/LineSpam/shaders/composite.cs.glsl
+++ src/projects/LineSpam/shaders/composite.cs.glsl
@@ -31,7 +31,7 @@ vec4 blendColor( in vec4 under, in vec4 over ) {
 	vec4 color = under;
 	color.a = max( over.a + color.a * ( 1.0f - over.a ), 0.001f );
 	color.rgb = over.rgb * over.a + color.rgb * color.a * ( 1.0f - color.a );
-	color.rgb /= color.a; // missing piece of a over b alpha blending - can't really see a lot of difference
+	color.rgb /= color.a;
 	return color;
 }
 
@@ -39,21 +39,22 @@ void main() {
 	const ivec2 loc = ivec2( gl_GlobalInvocationID.xy );
 
 	// opaque color comes from index loaded from id buffer, indexing into the SSBO for opaqueData[ id ].color
-	const int idx = imageLoad( idBuffer, loc ).r - 1;
-	vec4 opaqueColor = ( idx == 0 ) ? vec4( 0.0f ) : vec4( opaqueData[ idx ].color, 1.0f );
+	const uint idx = uint( imageLoad( idBuffer, loc ).r ) - 1;
+	vec4 opaqueColor = ( idx == 0 ) ? vec4( 0.0f ) : opaqueData[ idx ].color;
 
 	// transparent color resolve
-	vec4 tallySamples = vec4(
-		float( imageLoad( redTally, loc ).r ),
-		float( imageLoad( greenTally, loc ).r ),
-		float( imageLoad( blueTally, loc ).r ),
-		float( imageLoad( sampleTally, loc ).r )
+	vec4 tallySamples = vec4( // similar scheme to Icarus
+		float( imageLoad( redTally, loc ).r ) / ( 256.0f * 1024.0f ),
+		float( imageLoad( greenTally, loc ).r ) / ( 256.0f * 1024.0f ),
+		float( imageLoad( blueTally, loc ).r ) / ( 256.0f * 1024.0f ),
+		float( imageLoad( sampleTally, loc ).r ) / 256.0f
 	);
 	// averaging out colors and get a Beer's-law-ish term for density
-	vec4 transparentColor = vec4( tallySamples.rgb / tallySamples.a, exp( -0.003f * tallySamples.a ) );
+	// vec4 transparentColor = vec4( tallySamples.rgb / tallySamples.a, exp( -0.003f * tallySamples.a ) );
+	vec4 transparentColor = vec4( tallySamples.rgb, exp( -0.003f * tallySamples.a ) );
 
 	// blend the transparent over the opaque
-	vec3 finalColor = blendColor( opaqueColor, transparentColor );
+	vec3 finalColor = blendColor( opaqueColor, transparentColor ).rgb;
 
 	// store back in the composited result
 	imageStore( compositedResult, loc, vec4( finalColor, 1.0f ) );
--- src/projects/LineSpam/shaders/transparentDraw.cs.glsl
+++ src/projects/LineSpam/shaders/transparentDraw.cs.glsl
@@ -21,7 +21,54 @@ layout( binding = 1, std430 ) buffer transparentLineData {
 	line_t transparentData[];
 };
 
+void setPixelColor( int x, int y, vec4 color, float AAFactor ) {
+	// todo: add depth
+	imageAtomicAdd( redTally, ivec2( x, y ), uint( color.a * color.r * 1024 * AAFactor ) );
+	imageAtomicAdd( greenTally, ivec2( x, y ), uint( color.a * color.g * 1024 * AAFactor ) );
+	imageAtomicAdd( blueTally, ivec2( x, y ), uint( color.a * color.b * 1024 * AAFactor ) );
+	imageAtomicAdd( sampleTally, ivec2( x, y ), uint( color.a * AAFactor ) );
+}
+
+// line drawing - from https://zingl.github.io/bresenham.html
+void plotLineWidth( int x0, int y0, int x1, int y1, vec4 color, float wd ) {
+	int dx = abs( x1 - x0 ), sx = x0 < x1 ? 1 : -1;
+	int dy = abs( y1 - y0 ), sy = y0 < y1 ? 1 : -1;
+	int err = dx - dy, e2, x2, y2;	/* error value e_xy */
+	float ed = dx + dy == 0 ? 1 : sqrt( float( dx ) * float( dx ) + float( dy ) * float( dy ) );
+
+	for ( wd = ( wd + 1 ) / 2; ; ) { /* pixel loop */
+		setPixelColor( x0, y0, color, max( 0, 255 - 255 * ( abs( err - dx + dy ) / ed - wd + 1 ) ) );
+		e2 = err; x2 = x0;
+		if ( 2 * e2 >= -dx ) { /* x step */
+			for ( e2 += dy, y2 = y0; e2 < ed * wd && ( y1 != y2 || dx > dy ); e2 += dx )
+				setPixelColor( x0, y2 += sy, color, max( 0, 255 - 255 * ( abs( e2 ) / ed - wd + 1 ) ) );
+			if ( x0 == x1 ) break;
+			e2 = err; err -= dy; x0 += sx;
+		}
+		if ( 2 * e2 <= dy ) { /* y step */
+			for ( e2 = dx - e2; e2 < ed * wd && ( x1 != x2 || dx < dy ); e2 += dy )
+				setPixelColor( x2 += sx, y0, color, max( 0, 255 - 255 * ( abs( e2 ) / ed - wd + 1 ) ) );
+			if ( y0 == y1 ) break;
+			err += dx; y0 += sy;
+		}
+	}
+}
+
+#include "mathUtils.h"
+
 void main() {
 	uint index = gl_GlobalInvocationID.x + 4096 * gl_GlobalInvocationID.y;
 
+	line_t line = transparentData[ index ];
+
+	// translate NDC to screen coords
+	ivec2 iS = imageSize( depthBuffer );
+	float ratio = float( iS.x ) / float( iS.y );
+	ivec2 p0 = ivec2( int( RangeRemapValue( line.p0.x, -ratio, ratio, 0.0f, iS.x ) ), int( RangeRemapValue( line.p0.y, -1.0f, 1.0f, 0.0f, iS.y ) ) );
+	// uint d0 = uint( RangeRemapValue(  ) ); // depths
+	ivec2 p1 = ivec2( int( RangeRemapValue( line.p1.x, -ratio, ratio, 0.0f, iS.x ) ), int( RangeRemapValue( line.p1.y, -1.0f, 1.0f, 0.0f, iS.y ) ) );
+	// uint d1 = uint( RangeRemapValue(  ) );
+
+	// draw the line
+	plotLineWidth( p0.x, p0.y, p1.x, p1.y, line.color, 1.0 );
 }
