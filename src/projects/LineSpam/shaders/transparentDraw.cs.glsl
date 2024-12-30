77a5aaccdaa5e743457c0c0a3095d2a17141570d
jb <jon.baker1222@gmail.com>
2024-12-30 13:08:49 -0600

    depth checking for transparent writes

--- src/projects/LineSpam/shaders/transparentDraw.cs.glsl
+++ src/projects/LineSpam/shaders/transparentDraw.cs.glsl
@@ -10,6 +10,12 @@ layout( binding = 3, r32ui ) uniform uimage2D sampleTally;
 // depth buffer
 layout( binding = 4, r32ui ) uniform uimage2D depthBuffer;
 
+#include "mathUtils.h"
+
+uniform float depthRange;
+uniform mat4 transform;
+uniform uint numTransparent;
+
 // transparent line buffer
 struct line_t {
 	vec4 p0;
@@ -21,15 +27,23 @@ layout( binding = 1, std430 ) buffer transparentLineData {
 	line_t transparentData[];
 };
 
+// parameters for the lines
+ivec2 p0 = ivec2( 0 ), p1 = ivec2( 0 );
+uint d0 = 0, d1 = 0;
+
+// void setPixelColor( int x, int y, uint d, vec4 color, float AAFactor ) {
 void setPixelColor( int x, int y, vec4 color, float AAFactor ) {
-	// todo: add depth
-	imageAtomicAdd( redTally, ivec2( x, y ), uint( color.a * color.r * 1024 * AAFactor ) );
-	imageAtomicAdd( greenTally, ivec2( x, y ), uint( color.a * color.g * 1024 * AAFactor ) );
-	imageAtomicAdd( blueTally, ivec2( x, y ), uint( color.a * color.b * 1024 * AAFactor ) );
-	imageAtomicAdd( sampleTally, ivec2( x, y ), uint( color.a * AAFactor ) );
+	uint lerpedDepth = uint( mix( float( d0 ), float( d1 ), ( ( RangeRemapValue( x, p0.x, p1.x, 0.0f, 1.0f ) ) + ( RangeRemapValue( y, p0.x, p1.x, 0.0f, 1.0f ) ) ) / 2.0f ) );
+	if ( imageLoad( depthBuffer, ivec2( x, y ) ).r < lerpedDepth ) { // depth check
+		imageAtomicAdd( redTally, ivec2( x, y ), uint( color.a * color.r * 1024 * AAFactor ) );
+		imageAtomicAdd( greenTally, ivec2( x, y ), uint( color.a * color.g * 1024 * AAFactor ) );
+		imageAtomicAdd( blueTally, ivec2( x, y ), uint( color.a * color.b * 1024 * AAFactor ) );
+		imageAtomicAdd( sampleTally, ivec2( x, y ), uint( color.a * AAFactor ) );
+	}
 }
 
 // line drawing - from https://zingl.github.io/bresenham.html
+// void plotLineWidth( int x0, int y0, uint d0, int x1, int y1, uint d1, vec4 color, float wd ) {
 void plotLineWidth( int x0, int y0, int x1, int y1, vec4 color, float wd ) {
 	int dx = abs( x1 - x0 ), sx = x0 < x1 ? 1 : -1;
 	int dy = abs( y1 - y0 ), sy = y0 < y1 ? 1 : -1;
@@ -54,21 +68,26 @@ void plotLineWidth( int x0, int y0, int x1, int y1, vec4 color, float wd ) {
 	}
 }
 
-#include "mathUtils.h"
-
 void main() {
 	uint index = gl_GlobalInvocationID.x + 4096 * gl_GlobalInvocationID.y;
 
-	line_t line = transparentData[ index ];
+	if ( index < numTransparent ) {
+		line_t line = transparentData[ index ];
 
-	// translate NDC to screen coords
-	ivec2 iS = imageSize( depthBuffer );
-	float ratio = float( iS.x ) / float( iS.y );
-	ivec2 p0 = ivec2( int( RangeRemapValue( line.p0.x, -ratio, ratio, 0.0f, iS.x ) ), int( RangeRemapValue( line.p0.y, -1.0f, 1.0f, 0.0f, iS.y ) ) );
-	// uint d0 = uint( RangeRemapValue(  ) ); // depths
-	ivec2 p1 = ivec2( int( RangeRemapValue( line.p1.x, -ratio, ratio, 0.0f, iS.x ) ), int( RangeRemapValue( line.p1.y, -1.0f, 1.0f, 0.0f, iS.y ) ) );
-	// uint d1 = uint( RangeRemapValue(  ) );
+		// translate NDC to screen coords
+		ivec2 iS = imageSize( depthBuffer );
+		float ratio = float( iS.x ) / float( iS.y );
 
-	// draw the line
-	plotLineWidth( p0.x, p0.y, p1.x, p1.y, line.color, 1.0 );
+		// apply any desired transforms to p0 and p1
+		line.p0.xyz = ( transform * vec4( line.p0.xyz, 1.0f ) ).xyz;
+		line.p1.xyz = ( transform * vec4( line.p1.xyz, 1.0f ) ).xyz;
+
+		p0 = ivec2( int( RangeRemapValue( line.p0.x, -ratio, ratio, 0.0f, iS.x ) ), int( RangeRemapValue( line.p0.y, -1.0f, 1.0f, 0.0f, iS.y ) ) );
+		d0 = uint( RangeRemapValue( line.p0.z, -depthRange, depthRange, float( UINT_MAX ), 0.0f ) ); // depths
+		p1 = ivec2( int( RangeRemapValue( line.p1.x, -ratio, ratio, 0.0f, iS.x ) ), int( RangeRemapValue( line.p1.y, -1.0f, 1.0f, 0.0f, iS.y ) ) );
+		d1 = uint( RangeRemapValue( line.p1.z, -depthRange, depthRange, float( UINT_MAX ), 0.0f ) );
+
+		// draw the line
+		plotLineWidth( p0.x, p0.y, p1.x, p1.y, line.color, 1.5f );
+	}
 }
